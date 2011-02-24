3ec4727c192d68688acd139f724b6573fc9e0747
Hampton Catlin <hcatlin@gmail.com>
2011-02-24 17:01:40 +0000

    Instructions now track source line number and script name and pre-processed script value

--- parser/instruction.rb
+++ parser/instruction.rb
@@ -6,13 +6,24 @@ module Tritium
       attr :children, true
       attr :parent, true
       attr :root, true
+      attr :line_number
+      attr :line
+      attr :processed_line
     
-      def initialize(name = nil, args = [])
+      def initialize(name = nil, options = {})
+        # Primary Attributes
         @name = name
-        @args = args
+        @args = options[:args] || options["args"] || []
+        
+        # Default Setup
         @parent = nil
         @children = []
         @root = self
+
+        # Debug information
+        @line_number = options[:line_number] || options["line_number"] || 0
+        @line = options[:line] || options["line"] || ''
+        @processed_line = options[:processed_line] || options["processed_line"] || ''
       end
     
       def add(child)
