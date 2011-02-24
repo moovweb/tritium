f84e3b59ee2439f67e5d6e977e60ba3529b9594f
Hampton Catlin <hcatlin@gmail.com>
2011-02-24 15:08:55 +0000

    Adding in a lot more comments to the Reader class

--- parser/reader.rb
+++ parser/reader.rb
@@ -1,14 +1,29 @@
 require_relative 'instruction'
 
+# The Parser::Reader is what actually evals the script
+# and returns a tree of Instruction objects.
+#
+# Currently, the parser does *NO* validation.
+#
+# You must pass in an pre-processed script.
+
 module Tritium::Parser
   class Reader
-    def initialize()
+    # This is the method that actually runs the 
+    # parsing. The ruby interpreter runs the string
+    # and keeps hitting the method_missing method below
+    # for every instruction that goes through.
+    # 
+    # This actually returns an Instruction object that is
+    # the fully built parse tree.
+    #
+    # ONLY accepts a pre-processed Tritium script
+    def read(script_string)
       @root_instruction = Instruction.new
       @stack = [@root_instruction]
-    end
-    
-    def read(script_string)
+
       eval(script_string)
+
       @root_instruction
     end
     
@@ -17,6 +32,10 @@ module Tritium::Parser
       method_missing('select', *args, &block)
     end
    
+    # This is where the parsing magic happens.
+    # We hijack the ruby interpreter to do our parsing
+    # and take note of the stack with each block that
+    # gets called.
     def method_missing(name, *args, &block)
       ins = Instruction.new(name.to_s, args)
       @stack.last.add(ins)
