44fbc1a90e5b4deb70fe40d1aa5d97fc330ac7df
Hampton Catlin <hcatlin@gmail.com>
2011-02-28 17:31:37 +0000

    Reworked most of the instruction set stuff.

--- parser/instruction.rb
+++ parser/instruction.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 module Tritium
   module Parser
     class Instruction
@@ -6,34 +8,58 @@ module Tritium
       attr :children, true
       attr :parent, true
       attr :root, true
+      attr :scope
       attr :line_number
       attr :line
       attr :processed_line
-    
-      def initialize(name = nil, options = {})
+      
+      def self.root
+        Instruction.new("select", :args => ".", :scope => "Node")
+      end
+      
+      def self.map
+        @@map ||= YAML.load(File.open(File.dirname(__FILE__) + "/mappings.yml").read)
+      end
+      def map; self.class.map; end
+      def scope_map; @scope_map ||= map[scope]; end
+      def information; @information ||= parent.scope_map[@name]; end
+
+      def initialize(name, options = {})
         # Primary Attributes
         @name = name
-        @args = options[:args] || options["args"] || []
-        
+        @args   =[options[:args]   || options["args"]  || []].flatten
+        @root   = options[:root]   || options["root"]  || self
+        @parent = options[:parent] || options["parent"]
+        @scope  = options[:scope]  || options["scope"] || information["opens"] || @parent.scope
+
         # Default Setup
-        @parent = nil
         @children = []
-        @root = self
+        
+        if root?
+          # This should only really happen if you are root
+          @information = scope_map[name]
+        end
 
         # Debug information
         @line_number = options[:line_number] || options["line_number"] || 0
         @line = options[:line] || options["line"] || ''
         @processed_line = options[:processed_line] || options["processed_line"] || ''
       end
-    
-      def add(child)
-        child.parent = self
-        child.root = root
+
+      def add(name, options = {})
+        if scope_map[name].nil?
+          raise Invalid, "No such method '#{name}' allowed here!\nOnly allow: #{scope_map.keys.join(", ")}\n#{self.line}"
+        end
+        child = Instruction.new(name, options.merge(:root => self.root, :parent => self))
         children << child
+        child
       end
       
       def root?
         root == self
+      end
+      
+      class Invalid < StandardError
       end
     end
   end
