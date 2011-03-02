89ceadaf4dfefc06db629acafbb20e482a3f8648
Hampton Catlin <hcatlin@gmail.com>
2011-03-02 14:59:58 +0000

    really, really simple debug engine skeleton. Fails its test if I comment them in! Using the aforementioned engine functional tests.

--- engines/debug/engine.rb
+++ engines/debug/engine.rb
@@ -1,8 +1,18 @@
-require_relative '../base'
 
 module Tritium
-  module Engine
-    module Debug
+  module Engines
+    require_relative '../base'
+    class Debug < Engines::Base
+      require_relative 'step'
+
+      def run(xhtml_file, options = {})
+        env = options["env"] || options[:env] || {}
+        doc = @xml_parser.parse(xhtml_file)
+        
+        debug = {}
+        
+        doc
+      end
     end
   end
 end
