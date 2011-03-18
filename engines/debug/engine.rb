a20c6e8403355c1f8e65bd9ee2df4d6686bf3f22
Hampton Catlin <hcatlin@gmail.com>
2011-03-18 18:44:38 +0000

    Write out the parsed/expanded script.

--- engines/debug/engine.rb
+++ engines/debug/engine.rb
@@ -10,11 +10,19 @@ module Tritium
 
       def run(xhtml_file, options = {})
         env = options["env"] || options[:env] || {}
+        debug = !($TEST || !ENV["TRITIUM_DEBUG"] || !env["content_type"].include?("html"))
+        
+        if debug
+          # Write out the whole parsed script to the tmp folder for debugging!
+          File.open(File.join(@script_path, "../tmp/script.ts"), "w+") do |f|
+            f.write @root_instruction.to_script
+          end
+        end
         
         @root_step = Step::Text.new(@root_instruction)
         @root_step.execute(xhtml_file, env)
 
-        return @root_step.object if $TEST || !ENV["TRITIUM_DEBUG"] || !env["content_type"].include?("html")
+        return @root_step.object if !debug
         
 
         trace = File.join(@script_path, "../tmp/debug.json")
