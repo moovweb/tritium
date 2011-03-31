9e1142ee4ede2bd1fa8aa90475348867c4e4253f
Hampton Catlin <hcatlin@gmail.com>
2011-03-31 18:24:55 +0100

    fixing the $ test that andrew added

--- parser/preprocess.rb
+++ parser/preprocess.rb
@@ -36,7 +36,7 @@ module Tritium
       end
 
       def self.variable_expansion(script)
-        script.gsub(/([^\\]+)\$([^_\(][a-zA-Z_\-]*)/, '\1var(\'\2\')')
+        script.gsub(/([^\\]+)\$([^_\(\/, ][a-zA-Z0-9_\-]*)/, '\1var(\'\2\')')
       end
 
       # we can debug easier
