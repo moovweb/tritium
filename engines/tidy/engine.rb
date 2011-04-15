3c7d896d10240ed5079593d9611c8f8c66d98dd8
Zhigang Chen <zhigang.chen@moovweb.com>
2011-04-15 16:24:29 -0700

    add tiny clean

--- engines/tidy/engine.rb
+++ engines/tidy/engine.rb
@@ -1,11 +1,23 @@
+require "tidy_ffi"
 require_relative "../debug/engine"
 
 module Tritium
   module Engines
     class Tidy < Debug
+      TidyFFI::Tidy.default_options.char_encoding = 'utf8'
+      TidyFFI::Tidy.default_options.show_warnings = 1
+      TidyFFI::Tidy.default_options.show_errors = 1
 
       def run(xhtml_file, options={})
-        #TODO: run tidy on this first
+        env = options["env"] || options[:env] || {}
+        content_type_key = "content_type"
+        content_type_key = "Content_Type" if env.has_key?("Content_Type")
+        content_type_key = "Content_type" if env.has_key?("Content_type")
+        content_type = env[content_type_key] or nil
+        if content_type.start_with?("text/html")
+          orig_xhtml_file = xhtml_file
+          xhtml_file = TidyFFI::Tidy.new(orig_xhtml_file).clean
+        end
         super(xhtml_file, options)
       end
     end
