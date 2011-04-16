a0a81f1f84666e458f78bd6dab412d18f3111940
zhigangc <zhigangc@gmail.com>
2011-04-15 23:11:24 -0700

    add force-output to tidy options

--- engines/tidy/engine.rb
+++ engines/tidy/engine.rb
@@ -7,6 +7,8 @@ module Tritium
       TidyFFI::Tidy.default_options.char_encoding = 'utf8'
       TidyFFI::Tidy.default_options.show_warnings = 1
       TidyFFI::Tidy.default_options.show_errors = 1
+      TidyFFI::Tidy.default_options.force_output = 1
+      TidyFFI::Tidy.default_options.output_xhtml = 1
 
       def run(xhtml_file, options={})
         env = options["env"] || options[:env] || {}
@@ -14,9 +16,14 @@ module Tritium
         content_type_key = "Content_Type" if env.has_key?("Content_Type")
         content_type_key = "Content_type" if env.has_key?("Content_type")
         content_type = env[content_type_key] or nil
-        if content_type.start_with?("text/html")
+        #x_requested_with = env["X-Requested-With"] or nil
+        ### TODO we need to check if the request has "X-Requested-With" in the header
+        if content_type.start_with?("text/html") #and x_requested_with != "XMLHttpRequest"
+          start_t = Time.now.to_f
           orig_xhtml_file = xhtml_file
           xhtml_file = TidyFFI::Tidy.new(orig_xhtml_file).clean
+          end_t = Time.now.to_f
+          puts "tidy clean took", (end_t-start_t), "milisec"
         end
         super(xhtml_file, options)
       end
