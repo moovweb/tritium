873a573400f59187e0d4f32831d80b8c1e50e9dc
Hampton Catlin <hcatlin@gmail.com>
2011-05-13 18:03:05 +0100

    Require the tidy engine AND add a functional dir lookup.

--- lib/tritium.rb
+++ lib/tritium.rb
@@ -3,9 +3,14 @@ require 'nokogiri'
 require_relative 'engines/base'
 require_relative 'engines/debug/engine'
 require_relative 'engines/reference/engine'
+require_relative 'engines/tidy/engine'
 
 module Tritium
   def self.spec
     @@spec ||= YAML.load(File.read(File.dirname(__FILE__) + "/../spec.yml"))
+  end
+  
+  def self.functional_test_location
+    File.dirname(__FILE__) + "/../test/functional"
   end
 end
