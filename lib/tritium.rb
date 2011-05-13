552f6c6099a3e414f4b0cd549d6752cae1d11834
Hampton Catlin <hcatlin@gmail.com>
2011-05-13 19:19:07 +0100

    Move require methods around

--- lib/tritium.rb
+++ lib/tritium.rb
@@ -1,9 +1,6 @@
 
 require 'nokogiri'
-require_relative 'engines/base'
-require_relative 'engines/debug/engine'
-require_relative 'engines/reference/engine'
-require_relative 'engines/tidy/engine'
+
 
 module Tritium
   def self.spec
@@ -14,3 +11,8 @@ module Tritium
     File.dirname(__FILE__) + "/../test/functional"
   end
 end
+
+require_relative 'engines/base'
+require_relative 'engines/debug/engine'
+require_relative 'engines/reference/engine'
+require_relative 'engines/tidy/engine'
