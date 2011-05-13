fa5e33276005b923f9b59891c3c794fd9acf1c18
Hampton Catlin <hcatlin@gmail.com>
2011-05-13 19:23:21 +0100

    Make sure to explicitly specify the module for Parser

--- lib/parser/reader.rb
+++ lib/parser/reader.rb
@@ -7,65 +7,67 @@ require_relative 'instruction'
 #
 # You must pass in an pre-processed script.
 
-module Tritium::Parser
-  class Reader
+module Tritium
+  module Parser
+    class Reader
 
-    def initialize(logger=nil)
-      @logger = logger || Logger.new(STDOUT)
-    end
+      def initialize(logger=nil)
+        @logger = logger || Logger.new(STDOUT)
+      end
 
-    # This is the method that actually runs the 
-    # parsing. The ruby interpreter runs the string
-    # and keeps hitting the method_missing method below
-    # for every instruction that goes through.
-    # 
-    # This actually returns an Instruction object that is
-    # the fully built parse tree.
-    #
-    # ONLY accepts a pre-processed Tritium script
-    def read(script_string)
-      @root_instruction = Instruction.root
-      @stack = [@root_instruction]
+      # This is the method that actually runs the 
+      # parsing. The ruby interpreter runs the string
+      # and keeps hitting the method_missing method below
+      # for every instruction that goes through.
+      # 
+      # This actually returns an Instruction object that is
+      # the fully built parse tree.
+      #
+      # ONLY accepts a pre-processed Tritium script
+      def read(script_string)
+        @root_instruction = Instruction.root
+        @stack = [@root_instruction]
 
-      begin
-        eval(script_string)
-      rescue StandardError => e
-        msg =  "ERROR AT: #{@_script}:#{@_line_number}\n"
-        msg << "SCRIPT LINE: #{@_line}\n"
-        msg << "PROCESSED SCRIPT: #{@_processed_line}\n"
-        msg << @stack.inspect
-        @logger.error msg
+        begin
+          eval(script_string)
+        rescue StandardError => e
+          msg =  "ERROR AT: #{@_script}:#{@_line_number}\n"
+          msg << "SCRIPT LINE: #{@_line}\n"
+          msg << "PROCESSED SCRIPT: #{@_processed_line}\n"
+          msg << @stack.inspect
+          @logger.error msg
         
-        raise e
-      end
+          raise e
+        end
       
-      # Makes sure to de-parent and mark any arg-Instructions
-      @root_instruction.clean_args!
-      @root_instruction
-    end
-   
-    # This is where the parsing magic happens.
-    # We hijack the ruby interpreter to do our parsing
-    # and take note of the stack with each block that
-    # gets called.
-    def method_missing(name, *args, &block)
-      cmd(name, *args, &block)
-    end
-    
-    def cmd(name, *args, &block)
-      ins = @stack.last.add(name.to_s, :args => args, :processed_line => @_processed_line, :line => @_line, :line_number => @_line_number, :script_name => @_script)
-
-      if block
-        @stack.push(ins)
-        block.call(self, ins)
-        @stack.pop
+        # Makes sure to de-parent and mark any arg-Instructions
+        @root_instruction.clean_args!
+        @root_instruction
       end
-      ins
-    end
+   
+      # This is where the parsing magic happens.
+      # We hijack the ruby interpreter to do our parsing
+      # and take note of the stack with each block that
+      # gets called.
+      def method_missing(name, *args, &block)
+        cmd(name, *args, &block)
+      end
     
-    # For some reasons, select is defined on Object. Bastards.
-    def select(*args, &block)
-      cmd('select', *args, &block)
+      def cmd(name, *args, &block)
+        ins = @stack.last.add(name.to_s, :args => args, :processed_line => @_processed_line, :line => @_line, :line_number => @_line_number, :script_name => @_script)
+
+        if block
+          @stack.push(ins)
+          block.call(self, ins)
+          @stack.pop
+        end
+        ins
+      end
+    
+      # For some reasons, select is defined on Object. Bastards.
+      def select(*args, &block)
+        cmd('select', *args, &block)
+      end
     end
   end
 end
