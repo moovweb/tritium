726f956177c6885126919a89762e3945b1a855d2
Hampton Catlin <hcatlin@gmail.com>
2011-05-13 19:38:18 +0100

    forgot config

---
+++ lib/config.rb
@@ -0,0 +1,9 @@
+module Tritium
+  def self.spec
+    @@spec ||= YAML.load(File.read(File.dirname(__FILE__) + "/../spec.yml"))
+  end
+  
+  def self.functional_test_location
+    File.dirname(__FILE__) + "/../test/functional"
+  end
+end
