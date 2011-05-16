4599bfc6ddfa2375221dd734b2a72a08716ff004
Hampton Catlin <hcatlin@gmail.com>
2011-05-16 12:55:06 +0100

    Test that we actually get an invalid result from the tokenizer

--- test/parser/tokenizer_test.rb
+++ test/parser/tokenizer_test.rb
@@ -5,7 +5,18 @@ require_relative '../../lib/parser/tokenizer'
 
 class TokenizerTest < MiniTest::Unit::TestCase
   include Tritium::Parser
+  def get_tokenizer(file)
+    Tokenizer.new(File.join(File.dirname(__FILE__), "scripts/" + file))
+  end
+  
   def test_basic_script
-    tokenizer = Tokenizer.new(File.join(File.dirname(__FILE__), "scripts/basic.ts"))
+    tokenizer = get_tokenizer("basic.ts")
+    tokens = tokenizer.to_a
+  end
+  
+  def test_invalid_script
+    tokenizer = get_tokenizer("invalid.ts")
+    tokens = tokenizer.to_a
+    assert_equal tokens[1].class, Tokenizer::Error
   end
 end
