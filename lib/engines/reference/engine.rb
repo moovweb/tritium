85df597fe8d29527199869d2e4594c5f7abb23dc
Hampton Catlin <hcatlin@gmail.com>
2011-05-17 15:02:31 +0100

    Write down debug lines again in the reference engine

--- lib/engines/reference/engine.rb
+++ lib/engines/reference/engine.rb
@@ -13,7 +13,8 @@ module Tritium
         root_scope = Scope::Text.new(doc.dup)
         root_scope.logger = @logger
         root_scope.env.merge! env
-        root_scope.instance_eval(@root_instruction.to_script) #processed_script)
+        #puts @root_instruction.to_debug_script
+        root_scope.instance_eval(@root_instruction.to_debug_script)
 
         took = Time.now - start
         @logger.stats("Script took #{took} sec to process") if @logger.respond_to? :stats
--- lib/parser/instruction.rb
+++ lib/parser/instruction.rb
@@ -106,6 +106,14 @@ module Tritium
         result
       end
       
+      def to_debug_script
+        debug_script = []
+        self.to_script.split("\n").each_with_index do |line, index|
+          debug_script << "$line = #{index + 1}\n#{line}"
+        end
+        debug_script.join("\n")
+      end
+      
       def to_hash
         { :line_number => @line_number,
           :line => @line,
