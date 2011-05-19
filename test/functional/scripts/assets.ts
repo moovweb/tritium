92a5a26f6881683332589f80431881ed2e059f07
Hampton Catlin <hcatlin@gmail.com>
2011-05-19 20:21:52 +0100

    replicating issue #1

--- test/functional/scripts/assets.ts
+++ test/functional/scripts/assets.ts
@@ -1,5 +1,6 @@
 doc("html_fragment") {
   var("stylesheet", "myfile.css")
+  var("undefined")
   log(asset($stylesheet, "stylesheet"))
   top {
     insert_tag("link", src: asset($stylesheet, "stylesheet"))
