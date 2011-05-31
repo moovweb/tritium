44ebed4f6f655d862599b385fc96f618ba692c0c
Aaron Leung <aaron.leung@moovweb.com>
2011-05-30 17:00:39 -0700

    Removed the Error class (easier to use the Token class to represent errors, too). Also cleaned/refactored some stuff.

--- lib/parser/tokenizer.rb
+++ lib/parser/tokenizer.rb
@@ -1,28 +1,20 @@
 module Tritium
   module Parser
-    class Error
-      attr_reader :filename, :line_num, :message
-      
-      def initialize(filename, line_num, message)
-        @filename, @line_num, @message = filename, line_num, message
-      end
-  
-      def to_s
-        "Error in #{@filename}, line #{@line_num}: #{@message}"
-      end
-    end
-
     class Tokenizer
       class Token
-        attr_reader :lexeme, :value, :filename, :line_num
+        attr_reader :filename, :line_num, :lexeme, :value
 
-        def initialize(lexeme, value = nil, filename = nil, line_num = nil)
-          @lexeme, @value = lexeme, value
+        def initialize(filename, line_num, lexeme, value = nil)
           @filename, @line_num = filename, line_num
+          @lexeme, @value = lexeme, value
         end
 
         def to_s
-          @value ? "[#{@lexeme}: #{@value}]" : "[#{lexeme}]"
+          if @lexeme then
+            return @value ? "[#{@lexeme}: #{@value}]" : "[#{lexeme}]"
+          else
+            return "Error in #{@filename}, line #{@line_num}: #{@value}"
+          end
         end
       end
 
@@ -81,11 +73,21 @@ module Tritium
       end
       private :ignore_multicomment!
 
-      def munch_error!(msg)
-        error = Error.new(@filename, @line_num, msg)
-        next_line!
+      def munch_error!(msg, len = nil)
+        error = Token.new(@filename, @line_num, nil, msg)
+        if not len then
+          next_line!
+        else
+          @line[0,len] = ""
+        end
         return error
       end
+      private :munch_error!
+
+      def token(lexeme, value = nil)
+        Token.new(@filename, @line_num, lexeme, value)
+      end
+      private :token
 
       def munch!
         while @line do
@@ -98,38 +100,32 @@ module Tritium
             ignore_multicomment!
             next
           when @line[/^\]#/]
-            @line[0,2] = ""
-            return Error.new(@filename,
-                             @line_num,
-                             "unmatched comment terminator")
+            return munch_error!("unmatched comment terminator")
           when @line[/^#/]
             next_line!
             next
           when m = pop_match!(/^(=|,|\(|\)|\{|\})/)
-            return Token.new(@@symbols[m], nil, @filename, @line_num)
+            return token(@@symbols[m])
           when @line[/^("|'|\/)/]
             if m = pop_match!(@@string_matchers[@line[0]]) then
               m = eval m
-              return Token.new(Regexp === m ? :REGEXP : :STRING,
-                               m,
-                               @filename,
-                               @line_num)
+              return token(Regexp === m ? :REGEXP : :STRING, m)
             else
               return munch_error!("unterminated string or regexp #{@line}")
             end
           when m = pop_match!(/^\$\w+/)
-            return Token.new(:VAR, m[1, m.length], @filename, @line_num)
+            return token(:VAR, m[1, m.length])
           when m = pop_match!(/^[a-zA-Z_:][-\w:.]*:/)
-            return Token.new(:KWD, m[0, m.length-1], @filename, @line_num)
+            return token(:KWD, m[0, m.length-1])
           when m = pop_match!(/^\$|^[_a-zA-Z]\w*/)
-            return Token.new(:FUNC, m, @filename, @line_num)
+            return token(:ID, m)
           when m = pop_match!(/^@import\b/)
             @line.lstrip!
             if m = pop_match!(@@string_matchers['"']) or
                m = pop_match!(@@string_matchers["'"]) then
-              return Token.new(:IMPORT, eval(m), @filename, @line_num)
+              return token(:IMPORT, eval(m))
             elsif m = pop_match!(/^(\S)+/) then
-              return Token.new(:IMPORT, m, @filename, @line_num)
+              return token(:IMPORT, m)
             else
               return munch_error!("malformed import")
             end
@@ -146,8 +142,8 @@ module Tritium
 
       def pop!
         tmp, @lookahead = @lookahead, munch!
-        if Token === tmp and tmp.lexeme == :STRING then
-          while Token === @lookahead and @lookahead.lexeme == :STRING do
+        if tmp and tmp.lexeme == :STRING then
+          while @lookahead and @lookahead.lexeme == :STRING do
             tmp.value << @lookahead.value
             @lookahead = munch!
           end
@@ -156,8 +152,8 @@ module Tritium
       end
 
       def each
-        while token = pop! do
-          yield token
+        while t = pop! do
+          yield t
         end
       end
 
