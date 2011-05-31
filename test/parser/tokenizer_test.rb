75a8b08ed81e5ad1990786577e7c0b5561eac1c0
Aaron Leung <aaron.leung@moovweb.com>
2011-05-30 17:01:40 -0700

    Updated the tokenizer test to reflect changes in the tokenizer.

--- test/parser/tokenizer_test.rb
+++ test/parser/tokenizer_test.rb
@@ -15,19 +15,19 @@ class TokenizerTest < MiniTest::Unit::TestCase
   def test_basic_script
     tokenizer = get_tokenizer("basic.ts")
     tokens = tokenizer.to_a
-    #tokens.each { |token| puts token }
+    # tokens.each { |token| puts token }
   end
   
   def test_invalid_script
     tokenizer = get_tokenizer("invalid.ts")
     tokens = tokenizer.to_a
-    assert_equal tokens[1].class, Error
-    #tokens.each { |token| puts token }
+    assert_equal tokens[1].lexeme, nil
+    # tokens.each { |token| puts token }
   end
 
   def test_false_negatives_script
     tokenizer = get_tokenizer("false-negatives.ts")
     tokens = tokenizer.to_a
-    #tokens.each { |token| puts token }
+    # tokens.each { |token| puts token }
   end
 end
