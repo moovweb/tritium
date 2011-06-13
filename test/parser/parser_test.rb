c915559149cee4238653a95a95b9ed93bb493bcd
Aaron Leung <aaron.leung@moovweb.com>
2011-06-13 00:20:18 -0700

    Got the parser test up and running. Messed with tokenizer test.

---
+++ test/parser/parser_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require_relative '../../lib/parser/parser'
+
+class TokenizerTest < MiniTest::Unit::TestCase
+  include Tritium::Parser
+
+  def test_parser
+    path, name = "scripts", "false-negatives.ts"
+    script_string = File.read(File.join(path, name))
+    parser = Parser.new(script_string, filename: name, path: path)
+    output = parser.parse().to_s
+    ref = File.read("scripts/reference-output.ts")
+    # puts output
+    assert(output == ref)
+  end
+end
