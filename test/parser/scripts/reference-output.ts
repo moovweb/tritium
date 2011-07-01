efb29be1dd8109713b8116d2233163acdc4d18a7
Hampton Catlin <hcatlin@gmail.com>
2011-07-01 16:14:36 +0100

    This should be the reference output now.

--- test/parser/scripts/reference-output.ts
+++ test/parser/scripts/reference-output.ts
@@ -1,17 +1,27 @@
-# ENTERING FILE: false-negatives.ts
-var("x", whatever())
+.# ENTERING FILE: false-negatives.ts
+var("x") {
+  set(whatever())
+}
 doc() {
   select("html") {
     select("body") {
       # ENTERING FILE: import-1.ts
-      attribute("data-foo", "in-the-body")
+      attribute("data-foo") {
+        value() {
+          set("in-the-body")
+        }
+      }
       log("blah")
       # LEAVING FILE: import-1.ts
       move_here("some/long/path/to/something/split/across/multiple/lines/and/concatenated", "top") {
         wrap("div", :"data-ur-bleeble:blabble" => "bork")
         # ENTERING FILE: import 2.ts
         move_here(".//div[@class='something']") {
-          attribute("id", "algol")
+          attribute("id") {
+            value() {
+              set("algol")
+            }
+          }
           # ENTERING FILE: nested-import.ts
           attribute("style") {
             remove()
@@ -21,8 +31,17 @@ doc() {
         }
         # LEAVING FILE: import 2.ts
       }
-      attribute("class", fetch(fetch("where/is/this/coming/from?")))
-      add_class("something-else")
+      attribute("class") {
+        value() {
+          set(fetch(fetch("where/is/this/coming/from?")))
+        }
+      }
+      attribute("class") {
+        value() {
+          append(" ")
+          append("something-else")
+        }
+      }
       copy_here(".//img[not(@alt)]")
       # ENTERING FILE: ../scripts/import-3.ts
       bottom() {
