d7f660ff8bd30e8993f4c32e16b3369c988cedf1
Hampton Catlin <hcatlin@gmail.com>
2011-07-01 13:29:11 +0100

    More common in ruby to not use () when chaining

--- test/parser/parser_test.rb
+++ test/parser/parser_test.rb
@@ -8,7 +8,7 @@ class ParserTest < MiniTest::Unit::TestCase
     path, name = File.join(File.dirname(__FILE__), "scripts"), "false-negatives.ts"
     script_string = File.read(File.join(path, name))
     parser = Parser.new(script_string, filename: name, path: path)
-    output = parser.parse().to_s
+    output = parser.parse.to_s
     ref = File.read(File.join(path, "/reference-output.ts"))
     # puts output
     assert(output == ref)
