bffdf7ff862216434acaa4ff21e3b57de029e77a
Hampton Catlin <hcatlin@gmail.com>
2011-07-10 14:35:25 +0100

    using µs now (microseconds!) to measure searches. closes manhattan issue #35

--- lib/tritium/engines/legacy/debug/engine.rb
+++ lib/tritium/engines/legacy/debug/engine.rb
@@ -60,12 +60,12 @@ module Tritium
           end
         end
         
-        selectors = selectors.sort_by { |s| s.debug[:search_time] }
+        selectors = selectors.sort_by { |s| s.debug[:search_time] * 1000 }
         selectors.reverse!
         @logger.info "Slowest Searches:"
 
         selectors[0..8].each_with_index do |step, index|
-          @logger.info "##{index + 1} #{(step.debug[:search_time] * 1000).to_i}ms - #{step.instruction.args.first.inspect} in #{step.instruction.location}"
+          @logger.info "##{index + 1} #{(step.debug[:search_time] * 1000 * 1000).to_i}µs - #{step.instruction.args.first.inspect} in #{step.instruction.location}"
         end
       end
 
