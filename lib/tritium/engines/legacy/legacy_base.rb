f32d635b71f21d0d3a7a60c0df79e72bbd7240eb
Hampton Catlin <hcatlin@gmail.com>
2011-07-11 13:58:05 +0100

    You can now run the new parser with the env command PARSER=true

--- lib/tritium/engines/legacy/legacy_base.rb
+++ lib/tritium/engines/legacy/legacy_base.rb
@@ -21,9 +21,14 @@ module Tritium
       end
       
       def processed_script
-        #parsed_output = Parser::Parser.new(@script_string, filename: @script_name, path: @script_path).parse.to_s
-        #puts parsed_output
-        processed = Parser::Preprocess.run(@script_string, @script_path, @script_name)
+        script_to_run = @script_string.clone
+        if ENV["PARSER"]
+          script_to_run = Parser::Parser.new(@script_string, filename: @script_name, path: @script_path).parse.to_s
+          if ENV["SCRIPT"]
+            puts script_to_run
+          end
+        end
+        processed = Parser::Preprocess.run(script_to_run, @script_path, @script_name)
       end
       
       def to_script
