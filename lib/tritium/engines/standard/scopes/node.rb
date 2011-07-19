01cf607b03bb848c66d1e4c0f3cb037bd7540a6f
Hampton Catlin <hcatlin@gmail.com>
2011-07-19 15:06:47 +0100

    Implement a whole bunch of Node methods

--- lib/tritium/engines/standard/scopes/node.rb
+++ lib/tritium/engines/standard/scopes/node.rb
@@ -5,8 +5,34 @@ module Tritium
         def node_invocation(ins, ctx, pos_args, kwd_args)
           case ins.name
           when :select
-            doc = Context[ins, ctx.value.xpath(pos_args.first)]
-            run_children(ins, doc)
+            nodeset = ctx.value.xpath(pos_args.first)
+            nodeset.each do |node|
+              doc = Context[ins, node]
+              run_children(ins, doc)
+            end
+          when :inner
+            inner = Context[ins, ctx.value.inner_html]
+            run_children(ins, inner)
+            ctx.value.inner_html = inner.value
+          when :text
+            text = Context[ins, ctx.value.text]
+            run_children(ins, text)
+            ctx.value.text = text.value
+          when :remove
+            ctx.value.remove()
+          when :name
+            name = Context[ins, ctx.value.name]
+            run_children(ins, name)
+            ctx.value.name = name.value
+          when :attribute
+            name = pos_args.first
+            xml_attribute = ctx.value.attribute(name)
+            if xml_attribute.nil?
+              ctx.value[name] = ""
+              xml_attribute = ctx.value.attribute(name)
+            end
+            attribute = Context[ins, xml_attribute]
+            run_children(ins, attribute)
           else
             throw "Method #{ins.name} not implemented in Node scope"
           end
