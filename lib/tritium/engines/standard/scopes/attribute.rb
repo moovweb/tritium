5ef2b4644f812545ae43cbfd07bb344fab90b985
Hampton Catlin <hcatlin@gmail.com>
2011-07-19 15:07:02 +0100

    Attribute name() and value() are working

--- lib/tritium/engines/standard/scopes/attribute.rb
+++ lib/tritium/engines/standard/scopes/attribute.rb
@@ -3,10 +3,18 @@ module Tritium
     class Standard < Base
       module AttributeFunctions
         def attribute_invocation(ins, ctx, pos_args, kwd_args)
-          #case ins.name
-          #else
+          case ins.name
+          when :value
+            val = Context[ins, ctx.value.value]
+            run_children(ins, val)
+            ctx.value.value = val.value
+          when :name
+            val = Context[ins, ctx.value.name]
+            run_children(ins, val)
+            ctx.value.name = val.value
+          else
             throw "Method #{ins.name} not implemented in Attribute scope"
-          #end
+          end
         end
       end
     end
