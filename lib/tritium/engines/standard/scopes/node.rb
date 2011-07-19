7ac7365d5a25117845841cb99235be27de65e4cb
Hampton Catlin <hcatlin@gmail.com>
2011-07-19 14:38:22 +0100

    Implement select()

--- lib/tritium/engines/standard/scopes/node.rb
+++ lib/tritium/engines/standard/scopes/node.rb
@@ -3,10 +3,13 @@ module Tritium
     class Standard < Base
       module NodeFunctions
         def node_invocation(ins, ctx, pos_args, kwd_args)
-          #case ins.name
-          #else
+          case ins.name
+          when :select
+            doc = Context[ins, ctx.value.xpath(pos_args.first)]
+            run_children(ins, doc)
+          else
             throw "Method #{ins.name} not implemented in Node scope"
-          #end
+          end
         end
       end
     end
