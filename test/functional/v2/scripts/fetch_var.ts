2180482b3ce61c75ab6b88d26a21f622f0a0ffa6
Hampton Catlin <hcatlin@gmail.com>
2011-07-24 15:23:10 +0100

    aarons weird issue

--- test/functional/v2/scripts/fetch_var.ts
+++ test/functional/v2/scripts/fetch_var.ts
@@ -1,9 +1,12 @@
 xml() {
-  $("div") {
-    name("span")
-    var("toggler-id", fetch("text()"))
-    insert_before("div", class: "mw_acc_section") {
-      attribute("data-ur-id", var("toggler-id"))
+  $("body") {
+    #var("toggler_id", fetch("@mw_title"))
+    $("div") {
+      var("toggler_id", fetch("@mw_title"))
+      name("span")
+      insert_before("div", class: "mw_acc_section") {
+        attribute("data-ur-id", var("toggler_id"))
+      }
     }
   }
 }
