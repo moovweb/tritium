a1599593d097285044c6d9fb6d5d43d70669c656
Hampton Catlin <hcatlin@gmail.com>
2011-08-19 12:14:55 -0700

    Another stab at encoding fixes

--- lib/tritium/extensions/string.rb
+++ lib/tritium/extensions/string.rb
@@ -1,14 +1,14 @@
 class String
 
   def fix_encoding
-    if self.valid_encoding?
-      self
+    if self.encoding.to_s == "UTF-8"
+      if !self.valid_encoding?
+        return self.chars.map{|c| c.valid_encoding? ? c : '_?_'}.join
+      end
     else
-      #ic = Iconv.new('UTF-8', 'UTF-8//IGNORE')
-      #self.unpack('C*').pack('U*')
-      #ic.discard_ilseq = "?!?"
-      #ic.conv(self + "    ")[0..-4]
-      self.chars.map{|c| c.valid_encoding? ? c : '_?_'}.join
+      ic = Iconv.new('UTF-8', self.encoding.to_s + '//IGNORE')
+      return ic.conv(self + "    ")[0..-5]
     end
+    self
   end
 end
