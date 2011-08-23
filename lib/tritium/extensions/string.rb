546f52b748036671c41455fad934b112d31d89c2
Andrew Farmer <andrew@moovweb.com>
2011-08-23 15:41:28 -0700

    Revert "better fix for the character issues" Reverting all of Hampton's encoding fixes.
    This reverts commit 4c2bf1fee636560ff2d16acdcbc41bb086405ea4.

--- lib/tritium/extensions/string.rb
+++ lib/tritium/extensions/string.rb
@@ -4,11 +4,10 @@ class String
     if self.valid_encoding?
       self
     else
-      #ic = Iconv.new('UTF-8', 'UTF-8//IGNORE')
+      ic = Iconv.new('UTF-8', 'UTF-8//IGNORE')
       #self.unpack('C*').pack('U*')
-      #ic.discard_ilseq = "?!?"
-      #ic.conv(self + "    ")[0..-4]
-      self.chars.map{|c| c.valid_encoding? ? c : '_?_'}.join
+      ic.discard_ilseq = true
+      ic.conv(self + "    ")[0..-4]
     end
   end
 end
