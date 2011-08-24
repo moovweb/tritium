d489d7dd12c5e445b51b361c7b32d7e7b155763b
Andrew Farmer <andrew@moovweb.com>
2011-08-23 17:51:20 -0700

    use Nokogiri to guess the encoding of the page.  - most pages won't have to deal with this because either zlib can figure out the encoding or the encoding is specified in the content type header  - for the 1800 flowers page in question this takes 6 ms

--- lib/tritium/engines/standard/engine.rb
+++ lib/tritium/engines/standard/engine.rb
@@ -9,11 +9,25 @@ module Tritium
     class Standard < Base
       
       def run(input, options = {})
+        if input.encoding == Encoding::ASCII_8BIT
+          input.force_encoding(guess_encoding(input))
+        end
+
         run = Run.new(@logger, options)
         ctx = Context[@root_instruction, input.dup]
         run.process(@root_instruction, ctx)
         [ctx.value, run.export_vars]
       end
+
+      def guess_encoding(input)
+        s = Time.now
+        guess = Nokogiri::HTML.parse(input).encoding
+        x = Nokogiri::HTML.parse(input).to_html
+        raise Exception.new("Could not determine encoding") unless guess
+        @logger.warn("Spent #{(Time.now - s)*1000}ms figuring out the encoding: '#{guess}'")
+        guess
+      end
+
     end
   end
 end
