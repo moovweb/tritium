c681a8de952c571896205ac5035011a786796360
Andrew Farmer <andrew@moovweb.com>
2011-08-24 15:29:38 -0700

    a method of guessing the encoding that works even if the source page has specified an incorrect/invalid encoding

--- lib/tritium/engines/standard/engine.rb
+++ lib/tritium/engines/standard/engine.rb
@@ -8,24 +8,45 @@ module Tritium
   module Engines
     class Standard < Base
       
+      # TODO: consider having a project configuration that specifies
+      # which encodings should be guessed. It is possible for an encoding
+      # to be 'valid' but wrong.
+      GUESS_ENCODINGS = [Encoding::UTF_8, Encoding::ISO_8859_1]
+      # NOTE: The reason that 'ascii' is invalid is because tritium might
+      # add UTF-8 characters to the page, and we wouldn't be able to
+      # convert the page back to ASCII
+      INVALID_ENCODINGS = [Encoding::ASCII_8BIT, Encoding::ASCII]
+
+
       def run(input, options = {})
-        if input.encoding == Encoding::ASCII_8BIT
-          input.force_encoding(guess_encoding(input))
+        input = input.dup
+        if INVALID_ENCODINGS.include?(input.encoding) || !input.valid_encoding?
+          fix_encoding(input)
         end
 
         run = Run.new(@logger, options)
-        ctx = Context[@root_instruction, input.dup]
+        ctx = Context[@root_instruction, input]
         run.process(@root_instruction, ctx)
         [ctx.value, run.export_vars]
       end
 
-      def guess_encoding(input)
-        s = Time.now
-        guess = Nokogiri::HTML.parse(input).encoding
-        x = Nokogiri::HTML.parse(input).to_html
-        raise Exception.new("Could not determine encoding") unless guess
-        @logger.warn("Spent #{(Time.now - s)*1000}ms figuring out the encoding: '#{guess}'")
-        guess
+      #
+      # This is last-resort encoding detection:
+      # Guess a few encodings and see if the result is valid
+      #
+      def fix_encoding(input)
+        encoding = nil
+        GUESS_ENCODINGS.each do |e|
+          input.force_encoding(e)
+          if input.valid_encoding?
+            encoding = e
+            break
+          end
+        end
+
+        raise Exception.new("Failed to guess an encoding") unless encoding
+        @logger.warn("Missing or invalid encoding guessed as '#{encoding}'")
+        return nil
       end
 
     end
