b2ae147f7c37c7e09d78e7f09e363c12c3253d90
Andrew Farmer <andrew@moovweb.com>
2011-09-01 13:42:43 -0700

    tritium no longer has to worry about string encodings. tritium will always be handed a utf-8 string.  - this is better because part of the encoding determination has to be in quadrium anyway

--- lib/tritium/engines/standard/engine.rb
+++ lib/tritium/engines/standard/engine.rb
@@ -8,46 +8,11 @@ module Tritium
   module Engines
     class Standard < Base
       
-      # TODO: consider having a project configuration that specifies
-      # which encodings should be guessed. It is possible for an encoding
-      # to be 'valid' but wrong.
-      GUESS_ENCODINGS = [Encoding::UTF_8, Encoding::ISO_8859_1]
-      # NOTE: The reason that 'ascii' is invalid is because tritium might
-      # add UTF-8 characters to the page, and we wouldn't be able to
-      # convert the page back to ASCII
-      INVALID_ENCODINGS = [Encoding::ASCII_8BIT, Encoding::ASCII]
-
-
       def run(input, options = {})
-        input = input.dup
-        if INVALID_ENCODINGS.include?(input.encoding) || !input.valid_encoding?
-          fix_encoding(input)
-        end
-
         run = Run.new(@logger, options)
         ctx = Context[@root_instruction, input]
         run.process(@root_instruction, ctx)
         [ctx.value, run.export_vars]
-      end
-
-      #
-      # This is last-resort encoding detection:
-      # Guess a few encodings and see if the result is valid
-      #
-      def fix_encoding(input)
-        encoding = nil
-        GUESS_ENCODINGS.each do |e|
-          input.force_encoding(e)
-          if input.valid_encoding?
-            encoding = e
-            break
-          end
-        end
-
-        raise Exception.new("Failed to guess an encoding") unless encoding
-        # TODO: How can I print warnings without breaking Tritium tests?
-        #@logger.warn("Missing or invalid encoding guessed as '#{encoding}'")
-        return nil
       end
 
     end
