60840b37c8018505667279d25e567b9d0baee7d0
Hampton Catlin <hcatlin@gmail.com>
2011-09-06 16:27:41 +0100

    adding a comment to the else() test to make it make more sense

--- test/functional/v2/scripts/else.ts
+++ test/functional/v2/scripts/else.ts
@@ -1,3 +1,4 @@
+// Nothing is actually passed in. The variables are all blank. But, that's ok because its an else() that we are really testing
 match($x_moov_api) {
   with(/true/) {
     append("failed")
