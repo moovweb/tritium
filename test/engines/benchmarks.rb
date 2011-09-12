59ea31290454c1bdfdfa9561a54d3358a20763ee
Hampton Catlin <hcatlin@gmail.com>
2011-09-12 15:50:33 +0200

    adding in macys

--- test/engines/benchmarks.rb
+++ test/engines/benchmarks.rb
@@ -6,12 +6,21 @@ require 'rainbow'
 include Tritium::Engines
 require_relative '../../../nagual/lib/judy'
 
+ENV["CSV"] = "true"
+
 base_path = Tritium.functional_test_location
 
 log = Logger.new(STDOUT)
 log.level = Logger::ERROR
 
 totals = {}
+print("\nscript_name")
+engines = [Debug, Reference, Standard, Judy::Engine]
+engines.each do |engine_class|
+  print(",")
+  print(engine_class.name)
+end
+print("\n")
 
 Dir[base_path + "/scripts/*.ts"].each do |script_file_name|
   test_name = File.basename(script_file_name, ".ts")
@@ -34,18 +43,22 @@ Dir[base_path + "/scripts/*.ts"].each do |script_file_name|
     input = open(input_file_path).read
   end
   
-  print "#{test_name}:#{' ' * (25 - test_name.size)}"
+  if ENV["CSV"]
+    print test_name
+  else
+    print "#{test_name}:#{' ' * (25 - test_name.size)}"
+  end
 
   results = {}
   fastest_time = 10000000
   fastest_engine = nil
-  [Debug, Reference, Standard, Judy::Engine].each do |engine_class|
+  engines.each do |engine_class|
     begin
       tritium = engine_class.new(ts_script, :path => base_path + "/scripts", :script_name => test_name, :logger => log)
 
       totals[engine_class] ||= 0
       start = Time.now
-      50.times do 
+      100.times do 
         env_copy = env.dup
         # Run the input through the tritium script.
         result, export_vars = tritium.run(input, :env => env_copy)
@@ -62,13 +75,17 @@ Dir[base_path + "/scripts/*.ts"].each do |script_file_name|
     end
   end
   results.each do |engine, took|
-    took_string = took.to_s[0..5]
-    result = "(#{engine.name} #{took_string})"
-    size = result.size
-    if engine == fastest_engine
-      result = result.inverse
+    if ENV["CSV"]
+      print("," + took.to_s)
+    else
+      took_string = took.to_s[0..5]
+      result = "(#{engine.name} #{took_string})"
+      size = result.size
+      if engine == fastest_engine
+        result = result.underline
+      end
+      print("#{result}#{' ' * (20 - size)}")
     end
-    print("#{result}#{' ' * (20 - size)}")
   end
   print("\n")
 end
