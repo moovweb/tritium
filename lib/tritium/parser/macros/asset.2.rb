47cd0c55d493d6ee800d1d1479b6cff7b5e79e89
Hampton Catlin <hcatlin@gmail.com>
2011-10-27 17:01:43 -0700

    I AM IN YOUR TRITIUM, CLOSING YOUR ISSUE #183

--- lib/tritium/parser/macros/asset.2.rb
+++ lib/tritium/parser/macros/asset.2.rb
@@ -3,16 +3,23 @@
 ->(args) do
   file_name, type = args
   location = ""
+  puts "DEPRECATION WARNING: Don't use asset.2 anymore! Will be gone in 0.11"
   
   if type.value == "stylesheet"
     location = "/stylesheets/.css/"
-    puts "DEPRECATION WARNING: Please use sass('#{file_name.value[0..-5]}') instead" unless ENV["TEST"]
+    if file_name.respond_to?("value")
+      puts "DEPRECATION WARNING: Please use sass('#{file_name.value[0..-5]}') instead" unless ENV["TEST"]
+    end
   elsif type.value == "js"
     location = "/javascript/"
-    puts "DEPRECATION WARNING: Please use asset(\"javascripts/#{file_name.value}\") instead" unless ENV["TEST"]
+    if file_name.respond_to?("value")
+      puts "DEPRECATION WARNING: Please use asset(\"javascripts/#{file_name.value}\") instead" unless ENV["TEST"]
+    end
   elsif type.value == "image"
     location = "/images/"
-    puts "DEPRECATION WARNING: Please use asset(\"images/#{file_name.value}\") instead" unless ENV["TEST"]
+    if file_name.respond_to?("value")
+      puts "DEPRECATION WARNING: Please use asset(\"images/#{file_name.value}\") instead" unless ENV["TEST"]
+    end
   end
   "asset(concat(#{location.inspect}, #{file_name.to_script})) { yield() }"
 end
