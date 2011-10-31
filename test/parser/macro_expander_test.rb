578cb95496917cdb2c3186fea98afdf37a2ed5ba
Hampton Catlin <hcatlin@gmail.com>
2011-10-31 13:34:20 -0700

    Write a test to make sure that the last child of an ExpansionBlock knows its an arg if the ExpansionBlock is expected to return.

--- test/parser/macro_expander_test.rb
+++ test/parser/macro_expander_test.rb
@@ -6,11 +6,27 @@ class MacroTest < MiniTest::Unit::TestCase
   
   def setup
     @expander = MacroExpander.new
+    test_returning_macro = Macro.build_macro_from_string("log('a')\nconcat('wor', 'ked') { yield() }", "test", 0)
+    @expander.register_macro(test_returning_macro)
+  end
+  
+  def parse_script(script)
+    Parser.new(script, 
+               :expander => @expander).parse
   end
   
   def test_positionals
     macro = @expander.macros[[:insert_top, 1]]
     assert_equal %|insert_at("top", "hi") {\n  yield()\n}|, macro.expand("hi")
+  end
+  
+  def test_arg_settings
+    ins = parse_script("set(test())")
+    set = ins.statements.first
+    macro = set.pos_args.first
+    log, concat = macro.statements
+    assert !log.is_arg?
+    assert concat.is_arg?
   end
   
   def test_is_macro?
