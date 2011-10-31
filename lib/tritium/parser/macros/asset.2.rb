9ab99427466568fd1bf44507e6857e50c2d34307
Hampton Catlin <hcatlin@gmail.com>
2011-10-31 12:16:21 -0700

    Removing warnings in 1.1/m0.11

--- lib/tritium/parser/macros/asset.2.rb
+++ lib/tritium/parser/macros/asset.2.rb
@@ -3,23 +3,13 @@
 ->(args) do
   file_name, type = args
   location = ""
-  puts "DEPRECATION WARNING: Don't use asset.2 anymore! Will be gone in 0.11"
   
   if type.value == "stylesheet"
     location = "/stylesheets/.css/"
-    if file_name.respond_to?("value")
-      puts "DEPRECATION WARNING: Please use sass('#{file_name.value[0..-5]}') instead" unless ENV["TEST"]
-    end
   elsif type.value == "js"
     location = "/javascript/"
-    if file_name.respond_to?("value")
-      puts "DEPRECATION WARNING: Please use asset(\"javascripts/#{file_name.value}\") instead" unless ENV["TEST"]
-    end
   elsif type.value == "image"
     location = "/images/"
-    if file_name.respond_to?("value")
-      puts "DEPRECATION WARNING: Please use asset(\"images/#{file_name.value}\") instead" unless ENV["TEST"]
-    end
   end
   "asset(concat(#{location.inspect}, #{file_name.to_script})) { yield() }"
 end
