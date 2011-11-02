869445b30ed7ded4b2f00e1983190f6ad6a5449e
Hampton Catlin <hcatlin@gmail.com>
2011-11-02 13:35:21 -0700

    updating asset.2 to work with the new requirement to have asset-host end in a /

--- lib/tritium/parser/macros/asset.2.rb
+++ lib/tritium/parser/macros/asset.2.rb
@@ -3,20 +3,20 @@
 ->(args) do
   file_name, type = args
   location = ""
-  puts "DEPRECATION WARNING: Don't use asset.2 anymore! Will be gone in 0.11"
+  puts "DEPRECATION WARNING: Don't use asset.2 anymore! Will be gone in 1.0"
   
   if type.value == "stylesheet"
-    location = "/stylesheets/.css/"
+    location = "stylesheets/.css/"
     if file_name.respond_to?("value")
       puts "DEPRECATION WARNING: Please use sass('#{file_name.value[0..-5]}') instead" unless ENV["TEST"]
     end
   elsif type.value == "js"
-    location = "/javascript/"
+    location = "javascript/"
     if file_name.respond_to?("value")
       puts "DEPRECATION WARNING: Please use asset(\"javascripts/#{file_name.value}\") instead" unless ENV["TEST"]
     end
   elsif type.value == "image"
-    location = "/images/"
+    location = "images/"
     if file_name.respond_to?("value")
       puts "DEPRECATION WARNING: Please use asset(\"images/#{file_name.value}\") instead" unless ENV["TEST"]
     end
