811e9a9ee7194284998fb91aa1bd8c28d30b8b10
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2011-12-01 12:58:45 -0800

    Report version number

--- lib/tritium.rb
+++ lib/tritium.rb
@@ -1,6 +1,21 @@
 require 'nokogiri'
 
 module Tritium
+  class << self
+    def version
+      build_version_path = File.join(File.dirname(__FILE__), "../BUILD_VERSION")
+      major_version_path = File.join(File.dirname(__FILE__), "../VERSION")
+
+      version = nil
+      if File.exists?(build_version_path)
+        version = File.read(build_version_path).strip
+      elsif File.exists?(major_version_path)
+        version = File.read(major_version_path).strip + ".dev"
+      else
+        "0"
+      end
+    end
+  end
 end
 
 require_relative 'tritium/config'
