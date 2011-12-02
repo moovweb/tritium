fee5151c0a09164300fe7cdc0be1384d0b3665b9
Hampton Catlin <hcatlin@gmail.com>
2011-12-02 14:39:11 -0800

    reworking the capture tests a bit

--- test/functional/standard/text/regex_capture/main.ts
+++ test/functional/standard/text/regex_capture/main.ts
@@ -1,26 +1,5 @@
-xml()  {
-  select(".//*[@id='a']") {
-    select(".//*[@id='line1']") {
-      inner() {
-        replace("lamb.", "baby antelope.")
-        replace(".", "!")
-      }
-    }
-    select(".//*[@id='line2']") {
-      inner() {
-        replace("white as ", "")
-        replace("snow", "snowy")
-      }
-    }
-    select(".//*[@id='line3']") {
-      inner() {
-        replace(/(?<mary>Mary)/, concat("'", $mary, "'"))
-      }
-    }
-    select(".//*[@id='line4']") {
-      inner() {
-        replace(/(?<word>[^\s\.]+)(?<white_space>\s)/, concat($word, "y", $white_space))
-      }
-    }
-  }
-}
+// Put quotes around every instance of mary
+replace(/(?<mary>Mary)/, concat("'", $mary, "'"))
+
+// For all words (captured as $word), add a y at the end with the whitespace as defined in the $whitespace capture
+replace(/(?<word>[^\s\.]+)(?<white_space>\s)/, concat($word, "y", $white_space))
