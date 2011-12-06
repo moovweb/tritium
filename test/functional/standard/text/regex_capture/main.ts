16900f9b594db495054ced166aea167bdba74988
Hampton Catlin <hcatlin@gmail.com>
2011-12-06 13:11:20 -0800

    fix test

--- test/functional/standard/text/regex_capture/main.ts
+++ test/functional/standard/text/regex_capture/main.ts
@@ -2,6 +2,4 @@
 replace(/(?<mary>Mary)/, concat("'", $mary, "'"))
 
 // For all words (captured as $word), add a y at the end with the whitespace as defined in the $whitespace capture
-replace(/(?<word>[^\s\.]+)(?<white_space>\s)/) {
-  concat($word, "y", $white_space)
-}
+replace(/(?<word>[^\s\.]+)(?<white_space>\s)/, concat($word, "y", $white_space))
