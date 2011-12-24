95cac27a41ae4847785a883d54a7a5087cacb156
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2011-12-23 16:57:38 -0800

    Added error handling for no args

--- src/main.go
+++ src/main.go
@@ -4,16 +4,24 @@ import "os"
 import "tritium/packager"
 import "tritium/linker"
 
+func show_usage() {
+	println("General purpose Tritium command line interface. Commands are: package")
+}
+
 func main() {
-	command := os.Args[1]
-	if command == "package" {
-		pkg := packager.BuildDefaultPackage()
-		println(string(pkg.Marshal()))
-	} else if command == "link" {
-		println("Linking files found in the directory:", os.Args[2])
-		linker.RunLinker(os.Args[2])
+	if len(os.Args) > 1 {
+		command := os.Args[1]
+		if command == "package" {
+			pkg := packager.BuildDefaultPackage()
+			println(string(pkg.Marshal()))
+		} else if command == "link" {
+			println("Linking files found in the directory:", os.Args[2])
+			linker.RunLinker(os.Args[2])
+		} else {
+			println("No such command", command)
+			show_usage()
+		}
 	} else {
-		println("No such command ", command)
-		println("General purpose Tritium command line interface. Commands are: package")
+		show_usage()
 	}
 }
