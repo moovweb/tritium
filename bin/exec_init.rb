8c3f7760ae7f659fb91062b79bb2ce9401bb827d
Hampton Catlin <hcatlin@gmail.com>
2011-12-28 16:04:24 -0500

    Quick bundler exec'r.

---
+++ bin/exec_init.rb
@@ -0,0 +1,12 @@
+ENV["BUNDLE_GEMFILE"] = ::File.join(::File.dirname(__FILE__), "../Gemfile")
+require 'bundler'
+
+begin
+  Bundler.setup(:default)
+  Bundler.require(:default)
+  puts "Initialized..."
+rescue Exception => e
+  puts e.message
+  puts "Please make sure to run: bundle install"
+  exit(2)
+end
