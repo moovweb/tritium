d33de04f50d890f709d146fe1d18e6f527056637
Hampton Catlin <hcatlin@gmail.com>
2012-01-06 08:35:40 -0800

    Import checks now work.

--- src/tritium/proto/object.go
+++ src/tritium/proto/object.go
@@ -1,16 +1,17 @@
 package tritium
 
 import(
-	//proto "goprotobuf.googlecode.com/hg/proto"
+	proto "goprotobuf.googlecode.com/hg/proto"
 )
 
 func (obj *ScriptObject) Imports() ([]string) {
 	list := make([]string, 0)
 	obj.Root.Iterate(func(ins *Instruction) {
-		//if *ins.Type == Instruction_IMPORT {
-			//list = append(list, proto.GetString(ins.Value))
+		if *ins.Type == Instruction_IMPORT {
+			list = append(list, proto.GetString(ins.Value))
 			println("FOUND")
-		//}
+		}
+		println(*ins.Type)
 	})
 	return list
 }
