4079d3370110002148ae6940f7c0f2dec69efac3
Hampton Catlin <hcatlin@gmail.com>
2012-01-05 16:16:52 -0800

    Write a stub Go engine class

--- src/tritium/engine/engine.go
+++ src/tritium/engine/engine.go
@@ -2,10 +2,21 @@ package engine
 
 import(
 	tp "tritium/proto"
+	"libxml/xpath"
+	"rubex"
 )
 
 type Engine struct {
-	name string
+	RegexpCache map[string]*rubex.Regexp
+	XPathCache map[string]*xpath.Expression
+}
+
+func NewEngine() (*Engine) {
+	e := &Engine{
+		RegexpCache: make(map[string]*rubex.Regexp),
+		XPathCache: make(map[string]*xpath.Expression),
+	}
+	return e
 }
 
 func (eng *Engine) Run(transform *tp.Executable, input string, vars map[string]string) (data string, exports [][]string, logs []string) {
