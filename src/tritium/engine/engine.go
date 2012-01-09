6f89472e189ab22820d918c85f2958d1dfb011eb
Hampton Catlin <hcatlin@gmail.com>
2012-01-09 15:52:18 -0800

    New Engine API as discussed with Dr.Z today.

--- src/tritium/engine/engine.go
+++ src/tritium/engine/engine.go
@@ -9,19 +9,17 @@ import(
 type Engine struct {
 	RegexpCache map[string]*rubex.Regexp
 	XPathCache map[string]*xpath.Expression
-	Transform *tp.Executable
 }
 
-func NewEngine(transform *tp.Executable) (*Engine) {
+func NewEngine() (*Engine) {
 	e := &Engine{
 		RegexpCache: make(map[string]*rubex.Regexp),
 		XPathCache: make(map[string]*xpath.Expression),
-		Transform: transform,
 	}
 	return e
 }
 
-func (eng *Engine) Run(input string, vars map[string]string) (data string, exports [][]string, logs []string) {
+func (eng *Engine) Run(transform *tp.Executable, input string, vars map[string]string) (data string, exports [][]string, logs []string) {
 	data = ""
 	exports = make([][]string, 0)
 	logs = make([]string, 0)
