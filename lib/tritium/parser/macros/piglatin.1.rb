a320617b0cc1d9de88be0ec4bac9be8bd74db160
Hampton Catlin <hcatlin@gmail.com>
2012-01-10 09:03:27 -0800

    Useless old macros: DELETED.

--- lib/tritium/parser/macros/piglatin.1.rb
+++
@@ -1,9 +0,0 @@
-->(args) do
-  text = args.first
-  text = text.split(" ").collect do |word|
-    first_letter = word[0]
-    word[0..0] = ""
-    word << " #{first_letter}ay"
-  end.join(" ")
-  "#{text.inspect}"
-end
--- lib/tritium/parser/macros/replace.1.rb
+++
@@ -1,7 +0,0 @@
-->(args) do
-  what = args.first
-  use_method = ((what.returns == "Regexp") ? "regexp" : "text")
-%|replace_#{use_method}(#{what.to_script}) {
-  yield()
-}|
-end
