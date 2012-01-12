63de3ad3318254c43dfa4942fa267dabac4a970a
Hampton Catlin <hcatlin@gmail.com>
2012-01-12 12:44:27 -0800

    easy to use compile method

--- src/tritium/tritium.go
+++ src/tritium/tritium.go
@@ -1,1 +1,11 @@
 package tritium
+
+import(
+	"tritium/linker"
+	tp "tritium/proto"
+	"tritium/packager"
+)
+
+func Compile(file string) (*tp.Transform) {
+	return linker.RunWithPackage(file, packager.BuildDefaultPackage().Package)
+}
