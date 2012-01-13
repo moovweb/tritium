6410348903385021b5070b981479cf5fc735bab0
Hampton Catlin <hcatlin@gmail.com>
2012-01-13 11:58:12 -0800

    undoing my tweak.

--- packages/libxml/test/classic/variables/main.ts
+++ packages/libxml/test/classic/variables/main.ts
@@ -15,7 +15,7 @@ xml()  {
   }
 
   var("fifth", "worked")
-  match(var("first"), regexp("\^o\$")) { # Shouldn't match since this needs a full-match. Strings are automatically regexized
+  match(var("first"), "\^o\$") {
     var("fifth", "failed");
   }
 
