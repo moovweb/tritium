14c6c7d938109304d96d8fd97c54745ce0026f9e
Hampton Catlin <hcatlin@gmail.com>
2012-01-13 11:54:46 -0800

    Turn off panic handling for the moment… need a setting for this

--- src/tritium/spec/runner.go
+++ src/tritium/spec/runner.go
@@ -21,7 +21,7 @@ func All(directory string) {
 
 	for _, error := range(globalResult.Errors) {
 		if error.Panic {
-			Printf("Had panic in", error.Name)
+			Printf("Had panic in", error.Name, "\n", error.Message)
 		} else {
 			Printf("\n==========\n%v :: %v \n\n Got \n----------\n%v\n\n Expected \n----------\n%v\n", error.Name, error.Message, error.Got, error.Expected)
 		}
@@ -48,9 +48,9 @@ func (result *Result)Run(dir string, pkg *tp.Package, eng Engine) {
 	this_result := NewResult()
 	defer func() {
 			//log.Println("done")  // Println executes normally even in there is a panic
-			if x := recover(); x != nil {
-				this_result.Error(dir, Sprintf("run time panic: %v", x))
-			}
+			//if x := recover(); x != nil {
+			//	this_result.Error(dir, Sprintf("run time panic: %v", x))
+			//}
 			print(this_result.CharStatus())
 			result.Merge(this_result)
 		}()
