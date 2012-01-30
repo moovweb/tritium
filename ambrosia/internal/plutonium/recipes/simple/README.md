a0d2e76e0ee77bf86dd867dc655aa9c6253ed07e
Sean Jezewski <seanwjezewski@gmail.com>
2012-01-30 11:25:11 -0800

    Added plutonium mixer

---
+++ ambrosia/internal/plutonium/recipes/simple/README.md
@@ -0,0 +1,8 @@
+## Basics
+This project works with the Manhattan server
+See detailed Manhattan docs at http://manhattan-docs.heroku.com
+
+## Domains
+Remember to put all domains you're going to hit in your etc/hosts
+  127.0.0.1 	mlocal.{{#GOPERIOD#Domain}}
+  127.0.0.1 	mlocal.www.{{#GOPERIOD#Domain}}
---
+++ ambrosia/internal/plutonium/recipes/simple/scripts/mappings.ts
@@ -0,0 +1,41 @@
+
+/*
+  Mappings
+
+  Mappings are matchers that we use to determine if we should execute a
+  bit of Tritium during an execution. Aka, run something when we are
+  are on a certain page.
+
+  Example starting code:
+*/
+
+match($status) {
+
+  with(/302/) {
+    log("--> STATUS: 302")
+    // redirect: just let it go through
+  }
+
+  with(/200/) {
+    log("--> STATUS: 200")
+
+    match($path) {
+      with(/home/) {
+        // Include a log with every import to make it simple to know what scripts are running
+        log("--> Importing pages/homes.ts in mappings.ts")
+        @import pages/home.ts
+      }
+      else() {
+        log("--> Importing pages/not_homes.ts in mappings.ts")
+        //@import pages/not_home.ts
+      }
+    } //$path
+  } //200
+
+  else() {
+    // not 200 or 302 response status
+    @import pages/error.ts
+  }
+
+} //$status
+
