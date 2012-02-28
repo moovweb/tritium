0e8ad71c81716cccda34f3ae668c7491648b6c2f
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2012-02-28 10:20:16 -0800

    run even if we dont have children

--- src/athena/proto/instruction.go
+++ src/athena/proto/instruction.go
@@ -16,9 +16,6 @@ func (ins *Instruction) Iterate(itFunc func(*Instruction)) {
 
 func (ins *Instruction) IterateAll(itFunc func(*Instruction)) {
 	itFunc(ins)
-	if ins.Children == nil {
-		return
-	}
 	for _, child := range(ins.Arguments) {
 		child.Iterate(itFunc)
 	}
