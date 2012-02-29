6f84152c2044b47a33ea693d37ae3d1ff9c3f406
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2012-02-29 12:38:54 -0800

    repro of manhattan issue #288 -- for tarun, my love

---
+++ packages/libxml/test/bugs/fetch_modify/main.ts
@@ -0,0 +1,11 @@
+html() {
+  $("//div") {
+
+        $clickAction = fetch('./@id') {
+          prepend("x$('#")
+          append("').fire('click')")
+        }
+
+        log($clickAction)
+  }
+}
