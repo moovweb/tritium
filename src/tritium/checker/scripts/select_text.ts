4d07dcf9338a7b06938e33692e8aa0358c7215ba
Hampton Catlin <hcatlin@a002867f-d9b0-47ee-bdc3-c07d990ae810>
2012-03-01 11:57:45 -0800

    Much richer text()/comment() detection!

--- src/tritium/checker/scripts/select_text.ts
+++ src/tritium/checker/scripts/select_text.ts
@@ -1,5 +1,25 @@
+// warn 14 times
 html() {
-  $("div/text()")
-  select("div/text()")
-  var("text()")
+  $("div/text()") //warn
+  select("div/text()") //warn
+  $("text()") //warn
+  $("text()|script()") //warn
+  $("./a | ./text() | script()") //warn
+  $("script() | text()") //warn
+  $("./br|./text()") //warn
+  $("div/comment()") //warn
+  select("div/comment()") //warn
+  $("comment()") //warn
+  $("comment()|script()") //warn
+  $("./a | ./comment() | script()") //warn
+  $("script() | comment()") //warn
+  $("./br|./comment()") //warn
+  
+  
+  var("text()") 
+  $("//script[contains(text())]")
+  var("comment()") 
+  $("./span[@class='locatorSubhead'][contains(comment(), 'Events')]")
+  $("./span[@class='locatorSubhead'][contains(text(), 'Events')]")
+  $("//script[contains(comment())]")
 }
