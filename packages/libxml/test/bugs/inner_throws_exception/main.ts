4217d63785bea705b7fbb2ea9f1a0100e399c9aa
Sean Jezewski <sean@moovweb.com>
2012-03-16 15:50:56 -0700

    Updated failing inner test ... the syntax is not valid in 3.1

--- packages/libxml/test/bugs/inner_throws_exception/main.ts
+++ packages/libxml/test/bugs/inner_throws_exception/main.ts
@@ -4,14 +4,12 @@ html() {
 
     inner("<div></div>") # This is ok
     inner("<div></div><div></div>") # So is this
-
-    # But as soon as there are attributes, no good:
     inner("<div class='1'></div><div class='2 evaluated as html'></div>")
 
-
+    # But this is no good:
     $("./div[@class='1']"){
       add_class("first scoped")
-      text(set("<div></div>should be evaluated as text"))
+      text("<div></div>should be evaluated as text")
     }
   }
 }
