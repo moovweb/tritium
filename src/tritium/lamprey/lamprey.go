722d3713715ce7f3fe743a21ceda49bc3d133adf
Zhigang Chen <zhigang.chen@moovweb.com>
2012-03-16 16:07:17 -0700

    renamed; use the interface

--- src/tritium/lamprey/lamprey.go
+++ src/tritium/lamprey/lamprey.go
@@ -14,8 +14,9 @@ type Lamprey struct {
 }
 
 type Ctx struct {
-	*whale.Ctx
+	*whale.WhaleContext
 	*Lamprey
+	level int
 }
 
 func NewEngine(logger l4g.Logger) *Lamprey {
@@ -26,13 +27,14 @@ func NewEngine(logger l4g.Logger) *Lamprey {
 
 func (eng *Lamprey) Run(transform *tp.Transform, input interface{}, vars map[string]string) (output string, exports [][]string, logs []string) {
 	ctx := &Ctx{Lamprey: eng}
-	ctx.Ctx = whale.NewCtx(eng.Whale, vars, transform)
+	ctx.WhaleContext = whale.NewEngineCtx(eng.Whale, vars, transform)
 
 	ctx.Yields = append(ctx.Yields, &whale.YieldBlock{Vars: make(map[string]interface{})})
 	ctx.UsePackage(transform.Pkg)
 	scope := &whale.Scope{Value: input.(string)}
 	obj := transform.Objects[0]
 	ctx.Filename = proto.GetString(obj.Name)
+	ctx.level = 0
 	ctx.RunInstruction(scope, obj.Root)
 	output = scope.Value.(string)
 	exports = ctx.Exports
@@ -71,17 +73,24 @@ func (ctx *Ctx) RunInstruction(scope *whale.Scope, ins *tp.Instruction) (returnV
 		panic("Invalid instruction. Should have stopped before linking!")
 	}
 	indent := ""
-	for i := 0; i < len(ctx.Yields); i++ {
+	for i := 0; i < ctx.level; i++ {
 		indent += "\t"
 	}
+
+	ctx.level++
 
 	returnValue = ""
 	switch *ins.Type {
 	case tp.Instruction_BLOCK:
+		fmt.Printf("%s Instruction block\n", indent)
 		returnValue = ctx.RunChildren(scope, ins)
+		fmt.Printf("%s Instruction block returns: %v\n", indent, returnValue)
 	case tp.Instruction_TEXT:
+		fmt.Printf("%s Instruction text\n", indent)
 		returnValue = proto.GetString(ins.Value)
+		fmt.Printf("%s Instruction text returns: %v\n", indent, returnValue)
 	case tp.Instruction_LOCAL_VAR:
+		fmt.Printf("%s Eval local var\n", indent)
 		name := proto.GetString(ins.Value)
 		vars := ctx.Vars()
 		if len(ins.Arguments) > 0 {
@@ -101,13 +110,14 @@ func (ctx *Ctx) RunInstruction(scope *whale.Scope, ins *tp.Instruction) (returnV
 		ctx.Filename = proto.GetString(obj.Name)
 		ctx.RunChildren(scope, obj.Root)
 		ctx.Filename = curFile
+		fmt.Printf("%s Import: %q\n", indent, ctx.Filename)
 	case tp.Instruction_FUNCTION_CALL:
 		fun := ctx.Functions[int(proto.GetInt32(ins.FunctionId))]
 		args := make([]interface{}, len(ins.Arguments))
 		for i, argIns := range ins.Arguments {
 			args[i] = ctx.RunInstruction(scope, argIns)
 		}
-		debugInfo := fmt.Sprintf("calling %s(", fun.Name)
+		debugInfo := fmt.Sprintf("%s(", fun.Name)
 
 		for index, v := range args {
 			debugInfo += fmt.Sprintf("%q", v)
@@ -120,7 +130,7 @@ func (ctx *Ctx) RunInstruction(scope *whale.Scope, ins *tp.Instruction) (returnV
 
 		if proto.GetBool(fun.BuiltIn) {
 			if f := whale.LookupBuiltIn(fun.Name); f != nil {
-				returnValue = f(ctx.Ctx, scope, ins, args)
+				returnValue = f(ctx, scope, ins, args)
 			} else {
 				panic("missing function: " + fun.Name)
 			}
@@ -138,6 +148,7 @@ func (ctx *Ctx) RunInstruction(scope *whale.Scope, ins *tp.Instruction) (returnV
 			}
 			// PUSH!
 			ctx.PushYieldBlock(yieldBlock)
+			fmt.Printf("%s run children\n", indent)
 			returnValue = ctx.RunChildren(scope, fun.Instruction)
 			// POP!
 			ctx.PopYieldBlock()
@@ -147,6 +158,7 @@ func (ctx *Ctx) RunInstruction(scope *whale.Scope, ins *tp.Instruction) (returnV
 		fmt.Printf("%s returns: %v\n", indent, returnValue)
 		fmt.Printf("%s %s done\n\n", indent, fun.Name)
 	}
+	ctx.level--
 
 	return
 }
