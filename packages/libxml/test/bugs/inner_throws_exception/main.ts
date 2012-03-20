960db1166db39f0f955cf3d691ec020ded8f5518
Sean Jezewski <sean@moovweb.com>
2012-03-20 16:20:11 -0700

    Updated test w a tritium bug

--- packages/libxml/test/bugs/inner_throws_exception/main.ts
+++ packages/libxml/test/bugs/inner_throws_exception/main.ts
@@ -5,13 +5,14 @@ html() {
     inner("<div></div>") # This is ok
     inner("<div></div><div></div>") # So is this
 
-    # But as soon as there are attributes, no good:
     inner("<div class='1'></div><div class='2 evaluated as html'></div>")
-
 
     $("./div[@class='1']"){
       add_class("first scoped")
-      text(set("<div></div>should be evaluated as text"))
+      text() {
+        # This should be escaped but isn't
+        set("<div></div>should be evaluated as text")
+      }
     }
   }
 }
