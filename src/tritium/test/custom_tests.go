3d7822f34bb2fc116be2cecfdd23e9fbb6f4ed0b
Sean Jezewski <sean@moovweb.com>
2012-04-02 14:40:20 -0700

    Add custom test utils

---
+++ src/tritium/test/custom_tests.go
@@ -0,0 +1,48 @@
+package test
+
+import "testing"
+
+func RunCustomSuite(path string) {
+	test := func(t *testing.T) {
+		RunTestSuite(path, t)
+	}
+
+	tests = []testing.InternalTest{test}
+	benches, examples = initialize()
+
+	testing.Main(matchString, tests, benches, examples)
+}
+
+
+//func initialize() ([]testing.InternalTest, []testing.InternalBenchmark, []testing.InternalExample) {
+func initialize() ([]testing.InternalBenchmark, []testing.InternalExample) {
+//	tests := make([]testing.InternalTest, 0)
+//	tests = append(tests, testing.InternalTest{Name: "MyThing", F: TestMyThing})
+
+//	return testList(), make([]testing.InternalBenchmark,0), make([]testing.InternalExample,0)
+	return make([]testing.InternalBenchmark,0), make([]testing.InternalExample,0)
+}
+
+
+var matchPat string
+var matchRe *regexp.Regexp
+
+
+// func myMatchString(pat, str string) (result bool, err os.Error) { // go60.3
+func myMatchString(pat, str string) (result bool, err error) { //go1
+	fmt.Printf("Pat : (%v), string: (%v)\n", pat, str)
+
+	if matchRe == nil || matchPat != pat {
+//		matchPat = pat
+//		matchRe, err = regexp.Compile(matchPat)
+		matchRe, err = regexp.Compile(".*")
+		if err != nil {
+			return
+		}
+	}
+	result = matchRe.MatchString(str)
+	fmt.Printf("Result: (%v)\n", result)
+
+//	return result, nil
+	return true, nil
+}
