2a06d7d6def80f04016c627fbd733bb5f481ecce
Sean Jezewski <sean@moovweb.com>
2012-04-02 19:10:22 -0700

    Cleanup

--- src/tritium/test/custom_tests.go
+++ src/tritium/test/custom_tests.go
@@ -3,7 +3,6 @@ package test
 import (
 	"testing"
 	"regexp"
-	"fmt"
 )
 
 
@@ -13,42 +12,26 @@ func RunCustomSuite(path string) {
 	}
 
 	tests := []testing.InternalTest{testing.InternalTest{Name: "CustomTest",F: test,}}
-	benches, examples := initialize()
+	_, benches, examples := initialize()
 
 	testing.Main(matchString, tests, benches, examples)
 }
 
-
-//func initialize() ([]testing.InternalTest, []testing.InternalBenchmark, []testing.InternalExample) {
-func initialize() ([]testing.InternalBenchmark, []testing.InternalExample) {
-
-//	tests := make([]testing.InternalTest, 0)
-//	tests = append(tests, testing.InternalTest{Name: "MyThing", F: TestMyThing})
-
-//	return testList(), make([]testing.InternalBenchmark,0), make([]testing.InternalExample,0)
-	return make([]testing.InternalBenchmark,0), make([]testing.InternalExample,0)
+func initialize() ([]testing.InternalTest, []testing.InternalBenchmark, []testing.InternalExample) {
+	return make([]testing.InternalTest,0), make([]testing.InternalBenchmark,0), make([]testing.InternalExample,0)
 }
-
 
 var matchPat string
 var matchRe *regexp.Regexp
 
-
-// func myMatchString(pat, str string) (result bool, err os.Error) { // go60.3
 func matchString(pat, str string) (result bool, err error) { //go1
-	fmt.Printf("Pat : (%v), string: (%v)\n", pat, str)
-
 	if matchRe == nil || matchPat != pat {
-//		matchPat = pat
-//		matchRe, err = regexp.Compile(matchPat)
 		matchRe, err = regexp.Compile(".*")
 		if err != nil {
 			return
 		}
 	}
 	result = matchRe.MatchString(str)
-	fmt.Printf("Result: (%v)\n", result)
 
-//	return result, nil
 	return true, nil
 }
