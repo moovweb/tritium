1254a034a79335cf425b691b10a57a5564cb776c
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-05 22:52:01 -0700

    More work on slug API

--- athena/src/athena/proto/slug.go
+++ athena/src/athena/proto/slug.go
@@ -1,9 +1,50 @@
 package proto
 
-func NewSlug(name string, version string, stages int) *Slug {
-	slug := Slug{}
-	slug.Name = &name
-	slug.Version = &version
-	slug.Transformers = make([]*Transform, stages)
-	return &slug
+import (
+	"os"
+	"io/ioutil"
+	pb "goprotobuf.googlecode.com/hg/proto"
+)
+
+func NewSlug(name string, version string, stages int) (slug *Slug, err os.Error) {
+	slug = &Slug{
+		Name:         pb.String(name),
+		Version:      pb.String(version),
+		Transformers: make([]*Transform, stages),
+	}
+
+	return
+}
+
+func NewSlugFromFile(filename string) (slug *Slug, err os.Error) {
+	var data []byte
+
+	data, err = ioutil.ReadFile(filename)
+	if err != nil {
+		return
+	}
+
+	slug = &Slug{}
+	err = pb.Unmarshal(data, slug)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func (slug *Slug) WriteFile(filename string) (err os.Error) {
+	var data []byte
+
+	data, err = pb.Marshal(slug)
+	if err != nil {
+		return
+	}
+
+	err = ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		return
+	}
+
+	return
 }
