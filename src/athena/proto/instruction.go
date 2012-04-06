355715ec529da54b96f5d0604725517fe22204da
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-05 21:57:43 -0700

    Gofmt

--- src/athena/proto/instruction.go
+++ src/athena/proto/instruction.go
@@ -1,7 +1,7 @@
 package proto
 
 import (
-  pb "goprotobuf.googlecode.com/hg/proto"
+	pb "goprotobuf.googlecode.com/hg/proto"
 )
 
 func (ins *Instruction) Iterate(itFunc func(*Instruction)) {
@@ -9,7 +9,7 @@ func (ins *Instruction) Iterate(itFunc func(*Instruction)) {
 	if ins.Children == nil {
 		return
 	}
-	for _, child := range(ins.Children) {
+	for _, child := range ins.Children {
 		child.Iterate(itFunc)
 	}
 }
@@ -17,110 +17,110 @@ func (ins *Instruction) Iterate(itFunc func(*Instruction)) {
 func (ins *Instruction) IterateAll(itFunc func(*Instruction)) {
 	itFunc(ins)
 	if ins.Arguments != nil {
-		for _, child := range(ins.Arguments) {
+		for _, child := range ins.Arguments {
 			child.Iterate(itFunc)
 		}
 	}
 	if ins.Children != nil {
-		for _, child := range(ins.Children) {
+		for _, child := range ins.Children {
 			child.Iterate(itFunc)
 		}
 	}
 }
 
 func ListInstructions(instrs ...*Instruction) []*Instruction {
-  return append(make([]*Instruction, 0), instrs...)
+	return append(make([]*Instruction, 0), instrs...)
 }
 
 func FoldLeft(funcName string, base *Instruction, seq []*Instruction) (acc *Instruction) {
-  for acc = base; len(seq) > 0; seq = seq[1:] {
-    acc = MakeFunctionCall(funcName, ListInstructions(acc, seq[0]), nil, *base.LineNumber)
-  }
-  return acc
+	for acc = base; len(seq) > 0; seq = seq[1:] {
+		acc = MakeFunctionCall(funcName, ListInstructions(acc, seq[0]), nil, *base.LineNumber)
+	}
+	return acc
 }
 
 func MakeText(text string, lineNum int32) *Instruction {
-  return &Instruction {
-    Type: NewInstruction_InstructionType(Instruction_TEXT),
-    Value: pb.String(text),
-    LineNumber: pb.Int32(lineNum),
-  }
+	return &Instruction{
+		Type:       NewInstruction_InstructionType(Instruction_TEXT),
+		Value:      pb.String(text),
+		LineNumber: pb.Int32(lineNum),
+	}
 }
 
 func MakePosition(pos string, lineNum int32) *Instruction {
-  return &Instruction {
-    Type: NewInstruction_InstructionType(Instruction_POSITION),
-    Value: pb.String(pos),
-    LineNumber: pb.Int32(lineNum),
-  }
+	return &Instruction{
+		Type:       NewInstruction_InstructionType(Instruction_POSITION),
+		Value:      pb.String(pos),
+		LineNumber: pb.Int32(lineNum),
+	}
 }
 
 func MakeComment(comment string, lineNum int32) *Instruction {
-  return &Instruction {
-    Type: NewInstruction_InstructionType(Instruction_COMMENT),
-    Value: pb.String(comment),
-    LineNumber: pb.Int32(lineNum),
-  }
+	return &Instruction{
+		Type:       NewInstruction_InstructionType(Instruction_COMMENT),
+		Value:      pb.String(comment),
+		LineNumber: pb.Int32(lineNum),
+	}
 }
 
 func MakeImport(path string, lineNum int32) *Instruction {
-  return &Instruction {
-    Type: NewInstruction_InstructionType(Instruction_IMPORT),
-    Value: pb.String(path),
-    LineNumber: pb.Int32(lineNum),
-  }
+	return &Instruction{
+		Type:       NewInstruction_InstructionType(Instruction_IMPORT),
+		Value:      pb.String(path),
+		LineNumber: pb.Int32(lineNum),
+	}
 }
 
 func MakeLocalVar(name string, val *Instruction, block []*Instruction, lineNum int32) *Instruction {
-  node := &Instruction {
-    Type: NewInstruction_InstructionType(Instruction_LOCAL_VAR),
-    Value: pb.String(name),
-    Children: block,
-    LineNumber: pb.Int32(lineNum),
-  }
-  if val == nil {
-    node.Arguments = nil
-  } else {
-    node.Arguments = ListInstructions(val)
-  }
-  return node
+	node := &Instruction{
+		Type:       NewInstruction_InstructionType(Instruction_LOCAL_VAR),
+		Value:      pb.String(name),
+		Children:   block,
+		LineNumber: pb.Int32(lineNum),
+	}
+	if val == nil {
+		node.Arguments = nil
+	} else {
+		node.Arguments = ListInstructions(val)
+	}
+	return node
 }
 
 func MakeFunctionCall(name string, args []*Instruction, block []*Instruction, lineNum int32) *Instruction {
-  return &Instruction {
-    Type: NewInstruction_InstructionType(Instruction_FUNCTION_CALL),
-    Value: pb.String(name),
-    Arguments: args,
-    Children: block,
-    LineNumber: pb.Int32(lineNum),
-  }
+	return &Instruction{
+		Type:       NewInstruction_InstructionType(Instruction_FUNCTION_CALL),
+		Value:      pb.String(name),
+		Arguments:  args,
+		Children:   block,
+		LineNumber: pb.Int32(lineNum),
+	}
 }
 
 func MakeBlock(children []*Instruction, lineNum int32) *Instruction {
-  return &Instruction {
-    Type: NewInstruction_InstructionType(Instruction_BLOCK),
-    Children: children,
-    LineNumber: pb.Int32(lineNum),
-  }
+	return &Instruction{
+		Type:       NewInstruction_InstructionType(Instruction_BLOCK),
+		Children:   children,
+		LineNumber: pb.Int32(lineNum),
+	}
 }
 
-func (ins *Instruction) GetFunction(pkg *Package) (*Function) {
+func (ins *Instruction) GetFunction(pkg *Package) *Function {
 	funId := pb.GetInt32(ins.FunctionId)
 	return pkg.Functions[int(funId)]
 }
 
 func (instr *Instruction) Append(more ...*Instruction) {
-  if instr.Children == nil {
-    instr.Children = more
-  } else {
-    instr.Children = append(instr.Children, more...)
-  }
+	if instr.Children == nil {
+		instr.Children = more
+	} else {
+		instr.Children = append(instr.Children, more...)
+	}
 }
 
 func (instr *Instruction) ConcatList(more []*Instruction) {
-  instr.Append(more...)
+	instr.Append(more...)
 }
 
-func (instr *Instruction) ConcatBlock (more *Instruction) {
-  instr.Append(more.Children...)
+func (instr *Instruction) ConcatBlock(more *Instruction) {
+	instr.Append(more.Children...)
 }
--- src/athena/proto/package.go
+++ src/athena/proto/package.go
@@ -1,12 +1,12 @@
 package proto
 
-import(
+import (
 	"log"
 	pb "goprotobuf.googlecode.com/hg/proto"
 )
 
-func (pkg *Package) GetTypeId(name string) (int) {
-	for id, typ := range(pkg.Types) {
+func (pkg *Package) GetTypeId(name string) int {
+	for id, typ := range pkg.Types {
 		if pb.GetString(typ.Name) == name {
 			return id
 		}
@@ -14,11 +14,11 @@ func (pkg *Package) GetTypeId(name string) (int) {
 	return -1
 }
 
-func (pkg *Package) GetTypeName(id int32) (string) {
+func (pkg *Package) GetTypeName(id int32) string {
 	return pb.GetString(pkg.Types[id].Name)
 }
 
-func (pkg *Package) GetProtoTypeId(name *string) (*int32) {
+func (pkg *Package) GetProtoTypeId(name *string) *int32 {
 	scopeTypeId := 0
 
 	typeName := pb.GetString(name)
@@ -35,8 +35,8 @@ func (pkg *Package) GetProtoTypeId(name *string) (*int32) {
 }
 
 // TODO(SJ) : This should really return a list of descendants
-func (pkg *Package) FindDescendantType(thisType int32) int {	
-	for index, someType := range(pkg.Types) {
+func (pkg *Package) FindDescendantType(thisType int32) int {
+	for index, someType := range pkg.Types {
 		implements := pb.GetInt32(someType.Implements)
 		if implements == thisType && implements != 0 {
 			// The implements field defaults to 0. Base doesn't implement Base. Text doesn't implement Base
