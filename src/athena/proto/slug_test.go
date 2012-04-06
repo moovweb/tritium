db80bba49884bc81e939290df7d32aa322912c86
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-05 22:52:01 -0700

    More work on slug API

--- src/athena/proto/slug_test.go
+++ src/athena/proto/slug_test.go
@@ -2,25 +2,50 @@ package proto
 
 import "testing"
 
-func testNewSlug(t *testing.T, name string, version string, stages int) {
-	s := NewSlug(name, version, stages)
-	if s == nil {
+func checkSlug(t *testing.T, testslug *Slug, name string, version string, stages int) {
+	if testslug == nil {
 		t.Fatal("Slug is nil")
 	}
-	if len(s.Transformers) != stages {
+	if len(testslug.Transformers) != stages {
 		t.Fatal("Invalid number of segments")
 	}
-	if *s.Name != name || *s.Version != version {
+	if *testslug.Name != name || *testslug.Version != version {
 		t.Error("Expected slug named", name, "and version", version)
-		t.Error("Got slug named", *s.Name, "and version", *s.Version)
+		t.Error("Got slug named", *testslug.Name, "and version", *testslug.Version)
 		t.Fail()
 	}
 }
 
 func TestNewSlug(t *testing.T) {
-	testNewSlug(t, "null", "0.0.0", 0)
+	name := "test"
+	version := "0.0.0"
+	stages := 4
+
+	s, err := NewSlug(name, version, stages)
+	if err != nil {
+		t.Fatal("Error creating slug")
+	}
+	checkSlug(t, s, name, version, stages)
 }
 
-func TestNewSlugFourTransforms(t *testing.T) {
-	testNewSlug(t, "steepandcheap", "1.0.1", 4)
+func TestWriteSlug(t *testing.T) {
+	name := "test"
+	version := "0.0.0"
+	stages := 0
+
+	s, err := NewSlug(name, version, stages)
+	if err != nil {
+		t.Fatal("Error creating slug")
+	}
+	checkSlug(t, s, name, version, stages)
+
+	err = s.WriteFile("test.slug")
+	if err != nil {
+		t.Fatal("Error writing slug")
+	}
+
+	s, err = NewSlugFromFile("test.slug")
+	if err != nil {
+		t.Fatal("Error loading slug from file")
+	}
 }
