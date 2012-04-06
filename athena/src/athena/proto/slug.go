4c6ace6560ffeace46885c325be520857c424d95
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-04-05 23:39:02 -0700

    Add ability to get slug from remote url

--- athena/src/athena/proto/slug.go
+++ athena/src/athena/proto/slug.go
@@ -3,6 +3,7 @@ package proto
 import (
 	"os"
 	"io/ioutil"
+	"http"
 	pb "goprotobuf.googlecode.com/hg/proto"
 )
 
@@ -24,6 +25,37 @@ func NewSlugFromFile(filename string) (slug *Slug, err os.Error) {
 		return
 	}
 
+	slug = &Slug{}
+	err = pb.Unmarshal(data, slug)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func NewSlugFromURL(url string) (slug *Slug, err os.Error) {
+	var data []byte
+	var resp *http.Response
+
+	resp, err = http.Get(url)
+	if resp.StatusCode != 200 {
+		err = os.NewError("Couldn't load slug from " + url)
+		return
+	}
+	if err != nil {
+		return
+	}
+	
+	data, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	if len(data) == 0 {
+		err = os.NewError("Received blank slug")
+		return
+	}
+	
 	slug = &Slug{}
 	err = pb.Unmarshal(data, slug)
 	if err != nil {
