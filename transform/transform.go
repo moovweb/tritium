9e1bfb742fa98548863f86e3f062a863a6cee1f7
Zhigang Chen <zhigang.chen@moovweb.com>
2012-06-26 16:12:37 -0700

    move CompileTest out to manhattan/project (this whole thing will be soon be moved back to tritium)

--- transform/transform.go
+++ transform/transform.go
@@ -2,25 +2,11 @@ package transform
 
 import (
 	tp "tritium/proto"
-	"tritium/linker"
 	"tritium/packager"
 )
 
 func CompileString(data string, path string, pkg *tp.Package) (*tp.Transform, error) {
 	return linker.RunStringWithPackage(data, path, pkg)
-}
-
-//******************************************************************************
-// "...add a big comment saying its a stop gap till go1/new testing framework 
-// support" - Sean
-//******************************************************************************
-func CompileTest(test *tp.TritiumTest, path string, pkg *tp.Package) (err error) {
-	test_transform, err := linker.RunStringWithPackage(*test.Script, path, pkg)
-	if err != nil {
-		return err
-	}
-	test.Transformer = test_transform
-	return
 }
 
 func LoadFunctions(functionPath string, pack *tp.Package) {
