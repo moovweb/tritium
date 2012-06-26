a396521819c198b1b172b827e8f6b70ad8228961
Zhigang Chen <zhigang.chen@moovweb.com>
2012-06-26 10:12:25 -0700

    update mixer

--- proto/mixer.go
+++ proto/mixer.go
@@ -23,9 +23,6 @@ func NewMixer(path string) *Mixer {
 	return &Mixer{
 		Name:      pb.String(name),
 		Version:   pb.String(versionStr),
-		Assets:    make([]*File, 0),
-		Features:  make([]*Feature, 0),
-		Recipes:   make([]*Recipe, 0),
 		Rewriters: make([]*File, 0),
 		Package:   nil,
 	}
@@ -85,18 +82,6 @@ func OpenMixer(location string) (m *Mixer) {
 }
 
 func (m *Mixer) Inspect(printFunctions bool) {
-	fmt.Printf(" \tFeatures: \n\t\t%v\n", m.Features)
-
-	println("\tAssets:")
-	for _, asset := range m.Assets {
-		fmt.Printf("\t\t -- %v\n", pb.GetString(asset.Path))
-	}
-
-	println("\tRecipes:")
-	for _, recipe := range m.Recipes {
-		fmt.Printf("\t\t -- Recipe (%v) contains (%v) files.\n", pb.GetString(recipe.Name), len(recipe.Files))
-	}
-
 	println("\tRewriters:")
 	for _, rewriter := range m.Rewriters {
 		fmt.Printf("\t\t -- %v\n", pb.GetString(rewriter.Path))
@@ -111,18 +96,6 @@ func (m *Mixer) Unpack(path string) {
 	err := os.MkdirAll(fullPath, 0755)
 	if err != nil {
 		panic(err.Error())
-	}
-
-	err = m.unpackFiles(filepath.Join(fullPath, "assets"), m.Assets)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, recipe := range m.Recipes {
-		err = m.unpackFiles(filepath.Join(fullPath, "recipes", pb.GetString(recipe.Name)), recipe.Files)
-		if err != nil {
-			panic(err)
-		}
 	}
 
 	err = m.unpackFiles(filepath.Join(fullPath, "rewriters"), m.Rewriters)
