98ed465f55419b020a07d1486b4fce9966cb64b8
Manoj Dayaram <i.am.noj@gmail.com>
2012-06-28 11:56:03 -0700

    Removing slug from project, that's being moved back to manhattan.

--- transform/slug.go
+++ transform/slug.go
@@ -60,37 +60,3 @@ func NewSlugFromURL(url string) (slug *tp.Slug, err error) {
 
 	return
 }
-
-var defaultRewriters = [...]string{
-	"requests.ts",
-	"response_pre.ts",
-	"body.ts",
-	"response_post.ts"}
-
-func NewSlugFromProject(proj *project.Project, mixerPath string) (*tp.Slug, error) {
-	mxr := tp.OpenMixer(mixerPath)
-	rewriters, err := Generate(proj, mxr)
-	if err != nil {
-		return nil, err
-	}
-
-	slug, err := tp.NewSlug(proj.Name, proj.Version, len(defaultRewriters))
-	if err != nil {
-		return nil, err
-	}
-
-	userPackagePath := filepath.Join(proj.Path, "functions", "main.ts")
-	LoadFunctions(userPackagePath, mxr.Package)
-
-	for i, name := range defaultRewriters {
-		filename := filepath.Join(proj.ScriptPath, name)
-		if rewriters[name] == nil {
-			return nil, errors.New("Couldn't load rewriter: " + name)
-		}
-		slug.Transformers[i], err = CompileString(string(rewriters[name]), filename, mxr.Package)
-		if err != nil {
-			return nil, errors.New("Failed to create " + name + " transformer: " + err.Error())
-		}
-	}
-	return slug, nil
-}
