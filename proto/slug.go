822fe831306c62d55b48770a6921aacc058cb330
Manoj Dayaram <i.am.noj@gmail.com>
2012-07-05 11:11:34 -0700

    Fixed some failing test cases in proto

--- proto/slug.go
+++ proto/slug.go
@@ -2,7 +2,9 @@ package proto
 
 import (
 	pb "code.google.com/p/goprotobuf/proto"
+	"errors"
 	"io/ioutil"
+	"net/http"
 )
 
 func NewSlug(name string, version string, stages int) (slug *Slug, err error) {
@@ -24,6 +26,55 @@ func (slug *Slug) WriteFile(filename string) (err error) {
 	}
 
 	err = ioutil.WriteFile(filename, data, 0644)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func NewSlugFromFile(filename string) (slug *Slug, err error) {
+	var data []byte
+
+	data, err = ioutil.ReadFile(filename)
+	if err != nil {
+		return
+	}
+
+	slug = &Slug{}
+	err = pb.Unmarshal(data, slug)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func NewSlugFromURL(url string) (slug *Slug, err error) {
+	var data []byte
+	var resp *http.Response
+
+	resp, err = http.Get(url)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = errors.New("Couldn't load slug from " + url)
+		return
+	}
+
+	data, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	if len(data) == 0 {
+		err = errors.New("Received blank slug")
+		return
+	}
+
+	slug = &Slug{}
+	err = pb.Unmarshal(data, slug)
 	if err != nil {
 		return
 	}
