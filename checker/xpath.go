16d4e22adbd45ed4bcc16a3f2cffecdc6e8d1234
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-12 02:29:31 -0700

    Check all xpath based functions

--- checker/xpath.go
+++ checker/xpath.go
@@ -6,16 +6,30 @@ import (
 	tp "tritium/proto"
 )
 
+var xpath_funcs[] string = []string{
+	"copy_here",
+	"copy_to",
+	"fetch",
+	"move_here",
+	"move_to",
+	"remove",
+	"select",
+	"$",
+}
+
 func (result *CheckResult) CheckXpath(script *tp.ScriptObject) {
 	iterate(script, func(ins *tp.Instruction) {
 		if *ins.Type == tp.Instruction_FUNCTION_CALL {
 			name := null.GetString(ins.Value)
-			if name == "$" || name == "select" || name == "fetch" {
-				if ins.Arguments != nil {
-					test_xpath := null.GetString(ins.Arguments[0].Value)
-					err := xpath.Check(test_xpath)
-					if err != nil {
-						result.AddXpathWarning(script, ins, err.Error())
+			// These all take xpath as the first param
+			for _, xpath_func := range xpath_funcs {
+				if name == xpath_func {
+					if ins.Arguments != nil {
+						test_xpath := null.GetString(ins.Arguments[0].Value)
+						err := xpath.Check(test_xpath)
+						if err != nil {
+							result.AddXpathWarning(script, ins, err.Error())
+						}
 					}
 				}
 			}
