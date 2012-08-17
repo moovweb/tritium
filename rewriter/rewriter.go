533c47676037f3cf8b982665abcbaaa8995cd150
Zhigang Chen <zhigang.chen@moovweb.com>
2012-08-17 10:42:40 -0700

    this is obsolete

--- rewriter/rewriter.go
+++
@@ -1,8 +0,0 @@
-package rewriter
-
-type UrlRewriter interface {
-	AddRewriteRule(string, string) error
-	Done() error
-	RewriteUrl(string) (string, error) //rewrite original urls to m-urls
-	ReverseRewritUrle(string) (string, error) //rewrite m-urls to original urls
-}
