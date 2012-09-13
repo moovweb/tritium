b0a797ee1b7c0b6d4bf90d78cc364bba6933feb2
Michael JL Catlin <malrase@gmail.com>
2012-09-12 17:22:24 -0700

    Finishing off most of the node functions

--- packages/node/headers.tf
+++ packages/node/headers.tf
@@ -1,51 +1,51 @@
 
-" Returns the index of the last node scoped.  "
+" Returns the number of the current node in relation to its siblings. @example `$(\"./div\") { log(index()) }` will return '1' if the div is the first div child, '2' if it is the second div child, and so on. "
 @func index(Node %node) Text
 
-" Selects an element to pass it to a function. "
+" Selects an element to pass it to a function. @example Given `<div>Dog</div>`, `$(\"./div\") { $var = fetch(text()) }` will set 'var' to be 'Dog'. "
 @func Node.fetch(Text %selector) Text Text
 
-" Returns the current node "
+" Returns the current node. "
 @func Node.this() Node Node
 
-" Specifies the position of a node (e.g. `position(\"top\")` or `\"bottom\"`). "
+" Specifies the position of a node. @example `move_to(\"//body\", position(\"top\")` will move the current tag to the top of the body. "
 @func position(Text %position) Position
 
-//" Opens a scope for specifying the position of a node (e.g. `\"top\"` or `\"bottom\"`). For example, `position() { set(\"top\") }`. "
+//" Opens a scope for specifying the position (e.g. 'top' or 'bottom') of a node. @example `position() { set(\"top\") }`. "
 //@func position() Position
 
-" Searches the tree and selects all nodes matching **%xpath_selector**. "
+" Searches the tree and selects all nodes matching **%xpath_selector**. Works the same as `$`. @example `select(\"//div\")` will select every div in the HTML."
 @func Node.select(Text %xpath_selector) Text Node
 
-" Opens up the contents to text modification. Use only for text-only nodes - any XML children will be removed. "
+" Opens up the contents to text modification. Use only for text-only nodes - any XML children will be removed. @example With `<div> Dog <span>Cat</span> </div>`, the Tritium `$(\"./div\") { text() { prepend(\"Super\") } }` will result in `<div>Super Dog Cat</div>`."
 @func Node.text() Text Text
 
 " Moves the first node (**%what**) to the second node (**%where**), at a specified position (**%pos**). "
 @func Node.move(Node %what, Node %where, Position %pos) Text Node
 
-" Copies the node and yields to it "
+" Copies the node and yields to it. "
 @func Node.dup() Node Node
 
-" Opens a scope to rename the current node. For example, `name() { set(\"div\") }`. "
+" Opens a scope to rename the current node. @example `name() { set(\"div\") }` renames the current node to 'div'. "
 @func Node.name() Text Text
 
-" Removes the current node from the tree. "
+" Removes the current node from the tree. @example `$(\"//table\") { remove() }` will remove all the tables from the HTML. "
 @func Node.remove() Text Node
 
-" Returns the XPath of the current node. "
+" Returns the XPath of the current node. @example From any node, using `log(path())` will return the path to the node in the logs. "
 @func Node.path() Text Text
 
-//" Injects HTML (specified by **%html**) into the current node. For example, `inject(\"file.html\")`."
+//" Injects HTML (specified by **%html**) into the current node.  @example `inject(\"<div>New Div</div>\")` will insert your new HTML into the current node."
 //@func Node.inject(Text %html) Text
 
-" Creates a new node (specified by **%tag_name**) and inserts it at the location specified by **%pos**. For example, `insert_at(\"top\", \"div\")`. "
+" Creates a new node (specified by **%tag_name**) and inserts it at the location specified by **%pos**. @example `insert_at(\"top\", \"div\")` inserts a div at the top of the current node. "
 @func Node.insert_at(Position %pos, Text %tag_name) Text
 
-" Injects HTML (specified by **%html**) into the current node at the location specified by **%pos**. For example, `inject_at(\"top\", \"file.html\")`."
+" Injects HTML (specified by **%html**) into the current node at the location specified by **%pos**. @example `inject_at(\"top\", read(\"file.html\"))` injects the file specified at the top of the current node."
 @func Node.inject_at(Position %pos, Text %html) Text
 
-" Injects HTML (specified by **%html**) into the current node at the location specified by **%pos**. For example, `inject_at(\"top\", \"file.html\")`."
+" Injects HTML (specified by **%html**) into the current node at the location specified by **%pos**. @example `inject_at(\"top\", \"file.html\")` injects the file specified at the top of the current node."
 @func Node.inject_at(Text %pos, Text %html) Text
 
-" Replaces the current value with the one specified by %value. For example, `name() { set(\"div\") }`. "
+" Replaces the current value with the one specified by %value. @example `name() { set(\"div\") }`. "
 @func Node.set(Text %value) Text
