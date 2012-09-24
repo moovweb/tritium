1ce0a47c19b671befd948dd91834b38ceef78215
Naseem Makiya <naseem.makiya@moovweb.com>
2012-09-24 13:22:48 -0700

    base => asset function example added

---
+++ packages/base/test/examples/asset/main.ts
@@ -0,0 +1,4 @@
+# You would use this when the desktop server has an incorrect content-type in their Response Header that needs to be fixed in order to be parsed correctly. Because we match what tritium is applied using the content type. 
+$$("#desktop_img"){
+  attribute("src", asset("/images/moovweb_src"))
+}
