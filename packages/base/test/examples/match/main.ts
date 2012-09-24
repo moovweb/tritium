419385b2104dd388f40a19aac1edb4f532e667e8
Naseem Makiya <naseem.makiya@moovweb.com>
2012-09-24 14:30:39 -0700

    fixed match to have appropriate arguments

--- packages/base/test/examples/match/main.ts
+++ packages/base/test/examples/match/main.ts
@@ -1,15 +1,7 @@
 # Test added because the Reference engine gave an 'add_previous_sibling' error
 html() {
   $name = fetch(".//div[@id='my_div']/@id")
-  match($name){
-    with(/your_div/) {
-      log("We reached your div!")
-    }
-    with(/my_div/) {
+  match($name, "my_div"){
       log("We reached my div!")
-    }
-    else() {
-      log("Div not found.")
-    }
   }
 }
