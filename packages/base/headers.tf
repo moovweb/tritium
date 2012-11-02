a8b622526abd1b62c7536ee9657c3b949347f8fa
Naseem Makiya <naseem.makiya@moovweb.com>
2012-11-02 11:38:21 -0700

    example descriptions updated

--- packages/base/headers.tf
+++ packages/base/headers.tf
@@ -52,6 +52,11 @@
 # @scope Text,XMLNode
 # @args
 # @description 
+# The time function returns the time-to-execute the Tritium code up until it hits the function call. 
+# Things to note: The time taken varies for each implementation so to have an accurate sense of time several trials should be run. 
+# Common uses include:
+# 1) Optimizing Tritium script performance. 
+# In this example, we display how to log the time() output to the terminal using the log() function. 
 # @example
 # log(time())
 # @exampletext Tritium Tester Example
@@ -67,6 +72,14 @@
 # @scope Text
 # @args Text %target
 # @description
+# The match function is used for pseudo-logic in Tritium. With match(), you have the equivalent of if-else and case statements in many other programming languages. Match is used to test the content of variables with strings, regular expressions, and other variables and then run code according to whether or not the match is successful. 
+# Things to note: If you have more than one variable or regular expression to test againt, you can simply provide the match function with the variable in question and then use embeded with() statements for each case. You can also provide a final else() statement to serve as a catchall for all unsuccessful matches.
+# Common uses include:
+# 1) Matching the URL Path of the page and @importing different Tritium scripts depending on what page you are on.
+# 2) Matching attributes with certain content to determine if they need to be changed in some way. 
+# 3) Simulating if/then/else statements and boolean true/false logic to run differing Tritium. 
+# 4) Matching the status, content-type, or other information from the incoming header.
+# In this example, we match the $path variable to see if it matches the regular expression /product/. Since it does, the log "Match successful!" will output to your terminal.
 # @example
 #   $path = "product"
 #   match($path) {
@@ -90,6 +103,7 @@
 # @scope Text,XMLNode
 # @args Text %log_message
 # @description
+# The log function is used 
 # @example
 #   log("I've reached this point in the code!")
 # @exampletext Tritium Tester Example
