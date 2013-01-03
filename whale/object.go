9b89eaffa85101aa2bceabf14ec2b7a349078b03
Zhigang Chen <zhigang.chen@moovweb.com>
2013-01-03 15:46:05 -0800

    add clean func for cache objects

--- whale/object.go
+++ whale/object.go
@@ -2,6 +2,7 @@ package whale
 
 import "rubex"
 import "gokogiri/xpath"
+import "go-cache"
 
 type MemoryObject interface {
   Free()
@@ -16,10 +17,30 @@ type XpathExpObject struct {
 }
 
 func (o *RegexpObject) Size() int {
-	return len(o.String())
+	return 1
 }
 
 func (o *XpathExpObject) Size() int {
-	return len(o.String())
+	return 1
+}
+
+func CleanRegexpObject(object cache.CacheObject) error {
+	if o, ok := object.(*RegexpObject); ok {
+		if o.Regexp != nil {
+			o.Regexp.Free()
+			o.Regexp = nil
+		}
+	}
+	return nil
+}
+
+func CleanXpathExpObject(object cache.CacheObject) error {
+	if o, ok := object.(*XpathExpObject); ok {
+		if o.Expression != nil {
+			o.Expression.Free()
+			o.Expression = nil
+		}
+	}
+	return nil
 }
 
