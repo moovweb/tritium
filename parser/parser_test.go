488f155444f1a8fef7f7e642275cd5a8025ee491
Aaron Leung <aaron.leung@moovweb.com>
2013-01-08 17:48:38 -0800

    Revert "Making the unit tests pass."
    This reverts commit 1095a5c1baa093112ffc318cf08a62ba44eeadad.

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -2,11 +2,8 @@ package parser
 
 import (
 	. "testing"
-  "os"
-  "path/filepath"
 )
 
 func TestParseRun(t *T) {
-  wd, _ := os.Getwd()
-	ParseFile(filepath.Join(wd, "scripts", "two.ts"))
+	ParseFile("two.ts")
 }
