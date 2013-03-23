9b4a4ac14c7b3d5186478fe0b58329e69c9a2c08
Aaron Leung <aaron.leung@moovweb.com>
2013-03-22 17:04:40 -0700

    Starting the implementation of the new packaging flow.

--- packager/stub.go
+++ packager/stub.go
@@ -1,5 +1,11 @@
 package packager
 
-func id(x string) string {
+type Packager struct {
+  MixerDir      string
+  IncludePaths  []string
+  IsTransformer bool
+}
+
+func Id(x string) string {
   return x
 }
