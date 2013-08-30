417095071dc018fcfd3d4a105f6090779cafa06b
Aaron Leung <aaron.leung@moovweb.com>
2013-08-30 14:46:49 -0700

    rename this to match the pb name

--- protoface/transform.go
+++
@@ -1,9 +0,0 @@
-package protoface
-
-type Transform interface {
-	ProtoBuff
-	// func GetObjects() []*ScriptObject
-	func IGetNthObject(n int) ScriptObject
-	func ISetNthObject(n int, s ScriptObject)
-	func GetPkg() *Package
-}
