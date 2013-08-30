a9ed53226a331caeea9cde4f4e032c3abd294c1f
Aaron Leung <aaron.leung@moovweb.com>
2013-08-30 14:45:01 -0700

    Re-janked the transform interfaces.

--- proto/tritium_interface.go
+++ proto/tritium_interface.go
@@ -2,10 +2,14 @@ package proto
 
 import(
 	"slugtest/protoface"
-	)
+)
 
-func (m *Transform) IGetObjects() [] protoface.ScriptObject {
-	return m.GetObjects()
+func (m *Transform) IGetNthObject(n int) protoface.ScriptObject {
+	return m.GetObjects()[n]
+}
+
+func (m *Transform) ISetNthObject(n int, s protoface.ScriptObject) {
+	m.GetObjects()[n] = s
 }
 
 func (m *Transform) IGetPkg() protoface.Package {
--- protoface/transform.go
+++ protoface/transform.go
@@ -2,6 +2,8 @@ package protoface
 
 type Transform interface {
 	ProtoBuff
-	func GetObjects() []*ScriptObject
+	// func GetObjects() []*ScriptObject
+	func IGetNthObject(n int) ScriptObject
+	func ISetNthObject(n int, s ScriptObject)
 	func GetPkg() *Package
 }
