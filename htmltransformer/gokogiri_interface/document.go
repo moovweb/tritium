9b44123a1e148b50bbfee6a148f0b51ef61f9b1a
Wonja Fairbrother <wfairbrother@gmail.com>
2014-09-26 10:27:23 -0700

    document functions and cleanup

--- htmltransformer/gokogiri_interface/document.go
+++ htmltransformer/gokogiri_interface/document.go
@@ -2,38 +2,37 @@ package gokogiri_interface
 
 import (
 	"gokogiri/xml"
-	// ht "tritium/htmltransformer"
+	ht "tritium/htmltransformer"
 )
 
 type GokogiriXmlDocument struct {
 	xml.XmlDocument
 }
 
-// func (doc *GokogiriXmlDocument) CreateElementNode(tag string) ht.Node {
-// 	// return doc.XmlDoc.CreateElementNode(tag)
-// 	return &GokogiriXmlNode{doc.CreateElementNode(tag)}
-// }
+func (doc *GokogiriXmlDocument) CreateElementNode(tag string) ht.Node {
+	return &GokogiriXmlNode{doc.XmlDocument.CreateElementNode(tag)}
+}
 
-// func (doc *GokogiriXmlDocument) CreateCDataNode(data string) ht.Node {
-// 	return &GokogiriXmlNode{doc.CreateCDataNode(data)}
-// }
+func (doc *GokogiriXmlDocument) CreateCDataNode(data string) ht.Node {
+	return &GokogiriXmlNode{doc.XmlDocument.CreateCDataNode(data)}
+}
 
-// func (doc *GokogiriXmlDocument) CreateTextNode(data string) ht.Node {
-// 	return &GokogiriXmlNode{doc.CreateTextNode(data)}
-// }
+func (doc *GokogiriXmlDocument) CreateTextNode(data string) ht.Node {
+	return &GokogiriXmlNode{doc.XmlDocument.CreateTextNode(data)}
+}
 
-// func (doc *GokogiriXmlDocument) CreateCommentNode(data string) ht.Node {
-// 	return &GokogiriXmlNode{doc.CreateCommentNode(data)}
-// }
+func (doc *GokogiriXmlDocument) CreateCommentNode(data string) ht.Node {
+	return &GokogiriXmlNode{doc.XmlDocument.CreateCommentNode(data)}
+}
 
-// func (doc *GokogiriXmlDocument) CreatePINode(name, data string) ht.Node {
-// 	return &GokogiriXmlNode{doc.CreatePINode(name, data)}
-// }
+func (doc *GokogiriXmlDocument) CreatePINode(name, data string) ht.Node {
+	return &GokogiriXmlNode{doc.XmlDocument.CreatePINode(name, data)}
+}
 
-// func (doc *GokogiriXmlDocument) String() string {
-// 	return doc.String()
-// }
+func (doc *GokogiriXmlDocument) String() string {
+	return doc.XmlDocument.String()
+}
 
-// func (doc *GokogiriXmlDocument) Root() ht.Node {
-// 	return &GokogiriXmlNode{doc.Root()}
-// }
+func (doc *GokogiriXmlDocument) Root() ht.Node {
+	return &GokogiriXmlNode{doc.XmlDocument.Root()}
+}
